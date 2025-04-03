ab9030f03cd8af61aaff66496d1fd249f1b17893
Jake Wheeler <jake.wheeler@skylight.digital>
2025-04-03 09:45:07 -0400

    comments, tests

--- api/refiner/app/main.py
+++ api/refiner/app/main.py
@@ -142,11 +142,11 @@ async def refine_ecr(
 def _get_clinical_services(condition_codes: str) -> list[dict]:
     """
     This a function that loops through the provided condition codes. For each
-    condition code provided, it calls the trigger-code-reference service to get
-    the API response for that condition.
+    condition code provided, it returns the value set of clinical services associated
+    with that condition.
 
-    :param condition_codes: SNOMED condition codes to look up in TCR service
-    :return: List of API responses to check
+    :param condition_codes: SNOMED condition codes to look up in the TES DB
+    :return: List of clinical services associated with a condition code
     """
     clinical_services_list = []
     conditions_list = condition_codes.split(",")
