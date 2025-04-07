19be7974dc993a2cb583706db13660f7a733ae9a
Jake Wheeler <jake.wheeler@skylight.digital>
2025-04-07 09:07:23 -0400

    refactor: merge TES code into Refiner application (#19)

---
+++ refiner/tests/integration/test_message_refiner.py
@@ -0,0 +1,12 @@
+import httpx
+import pytest
+
+
+@pytest.mark.integration
+def test_health_check(setup):
+    """
+    Basic test to make sure the message refiner service can communicate with
+    other up and running services.
+    """
+    service_response = httpx.get("http://0.0.0.0:8080/")
+    assert service_response.status_code == 200
