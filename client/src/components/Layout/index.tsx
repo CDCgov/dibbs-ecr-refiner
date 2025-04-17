f598494d7c7df2d9afb1db6a071d2aa3f00c58b9
Jake Wheeler <jake.wheeler@skylight.digital>
2025-04-17 10:40:13 -0400

    add dibbs logo

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -1,23 +1,29 @@
 import { Link } from 'react-router';
+import DibbsLogo from '../../assets/dibbs-logo.svg';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="p-2 text-white bg-blue-500 min-h-screen flex flex-col gap-4">
+    <div className="p-2 text-white bg-blue-500 min-h-screen flex flex-col gap-10">
       <header className="flex gap-10 items-end">
-        <h1 className="text-2xl">DIBBs eCR Refiner</h1>
-        <nav className="flex gap-4">
+        <Link to="/">
+          <h1 className="flex gap-3">
+            <img src={DibbsLogo} alt="DIBBs" />
+            <span className="text-2xl">eCR Refiner</span>
+          </h1>
+        </Link>
+        {/* <nav className="flex gap-4">
           <Link className="p-1 hover:underline" to="/">
             Home
           </Link>
           <Link className="p-1 hover:underline" to="/about">
             About
           </Link>
-        </nav>
+        </nav> */}
       </header>
-      <main className="flex flex-1 p-6">{children}</main>
+      <main className="flex flex-1">{children}</main>
     </div>
   );
 }
