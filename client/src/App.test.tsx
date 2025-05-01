550356ed0365c509ed5be4a0b12b10efe1b7cd5c
Jake Wheeler <jake.wheeler@skylight.digital>
2025-05-01 10:02:02 -0400

    remove react query config

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,16 +1,11 @@
 import { render, screen } from '@testing-library/react';
-
 import App from './App';
 import { BrowserRouter } from 'react-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
 const renderApp = () => {
   return render(
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <App />
     </BrowserRouter>
   );
 };
