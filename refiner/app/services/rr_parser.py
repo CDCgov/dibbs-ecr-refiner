ff2e93b691612db5e7c1712a320813dc2ffbe0c4
robertmitchellv <robert.beaty.mitchell@gmail.com>
2025-05-06 15:42:31 -0700

    moved from xml -> lxml to be consistent with refine.py

--- refiner/app/services/rr_parser.py
+++ refiner/app/services/rr_parser.py
@@ -1,15 +1,17 @@
-import xml.etree.ElementTree as ET
+from typing import Any
 
 from fastapi import Response, status
+from lxml import etree
 
 NAMESPACES = {
     "cda": "urn:hl7-org:v3",
     "sdtc": "urn:hl7-org:sdtc",
     "voc": "http://www.lantanagroup.com/voc",
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance",
 }
 
 
-def parse_xml(rr_xml: str) -> ET.Element | Response:
+def parse_xml(rr_xml: str) -> Any | Response:
     """
     Parses a raw RR XML string and returns the root Element.
 
@@ -17,12 +19,13 @@ def parse_xml(rr_xml: str) -> ET.Element | Response:
         rr_xml (str): The raw XML string.
 
     Returns:
-        ElementTree.Element if successful, or FastAPI Response on error.
+        etree._Element if successful, or FastAPI Response on error.
     """
     try:
-        rr_root = ET.fromstring(rr_xml)
+        parser = etree.XMLParser(remove_blank_text=True)
+        rr_root = etree.fromstring(rr_xml.encode("utf-8"), parser=parser)
         return rr_root
-    except ET.ParseError as e:
+    except etree.ParseError as e:
         return Response(
             content=f"Failed to parse RR XML: {str(e)}",
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -34,16 +37,17 @@ def get_reportable_conditions(root) -> str | None:
     Scan the Report Summary section for SNOMED CT codes and return
     them as a comma-separated string, or None if none found.
     """
-    ns = {"cda": "urn:hl7-org:v3", "xsi": "http://www.w3.org/2001/XMLSchema-instance"}
     codes = []
-    for section in root.findall(".//cda:section", namespaces=ns):
-        if (c := section.find("cda:code", namespaces=ns)) is not None and c.attrib.get(
-            "code"
-        ) == "55112-7":
-            for ve in section.findall(
-                ".//cda:value[@codeSystem='2.16.840.1.113883.6.96']", namespaces=ns
-            ):
-                code = ve.attrib.get("code")
-                if code:
-                    codes.append(code)
+
+    # find sections with loinc code 55112-7
+    for section in root.xpath(
+        ".//cda:section[cda:code/@code='55112-7']", namespaces=NAMESPACES
+    ):
+        # find all values with the specified codeSystem
+        values = section.xpath(
+            ".//cda:value[@codeSystem='2.16.840.1.113883.6.96']/@code",
+            namespaces=NAMESPACES,
+        )
+        codes.extend(values)
+
     return ",".join(codes) if codes else None
