1f1a37e4e3dc85356ac87337bb47694331e0bd03
Jake Wheeler <jake.wheeler@skylight.digital>
2025-05-21 10:39:58 -0400

    pin sass embedded to 1.89.0, disable deprecation warnings

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
+const uswdsIncludePaths = [
+  'node_modules/@uswds',
+  'node_modules/@uswds/uswds/packages',
+];
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -12,6 +17,12 @@ export default defineConfig({
     setupFiles: 'tests/setup.ts',
   },
   css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true, // tons of deprectation warnings caused by uswds
+        loadPaths: uswdsIncludePaths,
+      },
+    },
     modules: {
       localsConvention: 'camelCaseOnly',
     },
