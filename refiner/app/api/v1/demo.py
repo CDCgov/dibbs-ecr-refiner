eb014d5e8c76ad03d9551ca7493f563226379d8f
Lina Roth <lina@skylight.digital>
2025-05-23 11:41:13 -0400

    remove log

--- refiner/app/api/v1/demo.py
+++ refiner/app/api/v1/demo.py
@@ -189,8 +189,6 @@ async def demo_upload(
         )
         _update_file_store(output_file_name, output_file_path, token)
 
-        print(rr_results)
-
         return JSONResponse(
             content=jsonable_encoder(
                 {
