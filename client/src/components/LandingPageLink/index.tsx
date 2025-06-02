dc191da6f8ca1daf04adf23711ed7aa1a545f35b
Jake Wheeler <jake.wheeler@skylight.digital>
2025-06-02 14:21:03 -0400

    fix attribute names

--- client/src/components/LandingPageLink/index.tsx
+++ client/src/components/LandingPageLink/index.tsx
@@ -15,8 +15,8 @@ export function LandingPageLink() {
         className="fill-blue-300"
       >
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z"
         />
       </svg>
