8665c20e6aa295417d92f41dce81dd82921cbd41
Jake Wheeler <jake.wheeler@skylight.digital>
2025-06-03 11:59:28 -0400

    wip home page updates

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import PlaceholderImg from '../../assets/home/placeholder.png';
 
 export function Home() {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col">
       <div className="p-10">
         <header className="flex items-center">
           <Link to="/">
@@ -15,25 +15,18 @@ export function Home() {
           </Link>
         </header>
       </div>
-      <main>
-        <div className="flex flex-col items-center justify-center gap-16 p-6 md:flex-row md:p-16">
+      <main className="flex grow flex-col">
+        <div className="flex flex-col items-center justify-center gap-16 md:flex-row">
           <div className="flex flex-col gap-8">
-            <div className="flex max-w-[28rem] flex-col items-start gap-4">
-              <h1 className="font-merriweather text-4xl font-bold">
+            <div className="flex max-w-[36rem] flex-col items-start gap-4">
+              <h1 className="font-merriweather !m-0 !text-5xl font-bold">
                 Focus on what matters.
               </h1>
-              <p className="text-xl font-normal">
-                This demo application is a proof-of-concept for the eCR Refiner,
-                a collaborative effort between the Centers for Disease Control
-                (CDC) and the Association of Public Health Labs (APHL) to create
-                a centrally-accessible web application for eCR refinement hosted
-                on APHL's Information Management System (AIMS). The eCR Refiner
-                tailors each case report to retain only the data jurisdictions
-                need — providing tools for reducing file size, increasing data
-                relevancy, and safeguarding sensitive patient information. The
-                filtering capability showcased in this proof-of-concept will be
-                expanded to include user-defined elements such as custom codes,
-                section-based filtering, and time-based filtering.
+              <p className="text-2xl font-normal">
+                eCR Refiner tailors each case report to retain only the data
+                jurisdictions need — providing tools for reducing file size,
+                increasing data relevancy, and safeguarding sensitive patient
+                information.
               </p>
             </div>
             <div>
@@ -54,10 +47,10 @@ export function Home() {
 
 function HowItWorks() {
   return (
-    <div className="flex flex-col items-center gap-6 gap-13 bg-blue-100 px-6 py-20 lg:px-34">
+    <div className="flex grow flex-col items-center gap-6 gap-13 bg-blue-100 px-6 py-20 lg:px-34">
       <h2 className="text-4xl font-bold text-black">How it works</h2>
       <ol className="flex flex-col gap-10 md:flex-row md:justify-between">
-        <li className="flex items-start gap-6">
+        <li className="flex w-1/3 items-start gap-6">
           <Number>1</Number>
           <p>
             This early conceptual demonstration of the power of the eCR Refiner
@@ -65,7 +58,7 @@ function HowItWorks() {
             reportability response (RR) file pair.
           </p>
         </li>
-        <li className="flex items-start gap-6">
+        <li className="flex w-1/3 items-start gap-6">
           <Number>2</Number>
           <p>
             The Refiner extracts the reportable condition(s) from the RR and
@@ -73,7 +66,7 @@ function HowItWorks() {
             Exchange Service (TES) for each reportable condition.
           </p>
         </li>
-        <li className="flex items-start gap-6">
+        <li className="flex w-1/3 items-start gap-6">
           <Number>3</Number>
           <p>
             The Refiner splits the original eICR into child eICRs - one per
