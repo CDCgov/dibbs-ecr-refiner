a4461b5987c075dcf0045e0ecb3fb0638f551511
Jake Wheeler <jake.wheeler@skylight.digital>
2025-06-11 09:41:01 -0400

    add titles

--- client/src/pages/Demo/ReportableConditions.tsx
+++ client/src/pages/Demo/ReportableConditions.tsx
@@ -11,29 +11,34 @@ export function ReportableConditions({
   onClick,
 }: ReportableConditionsProps) {
   return (
-    <Container color="blue">
-      <Content className="flex flex-col items-center">
-        <img className="p-3" src={InformationSvg} alt="Information icon" />
-        <div className="flex flex-col items-center gap-10">
-          <div className="flex flex-col items-center gap-6">
-            <div className="flex flex-col items-center gap-3">
-              <p className="text-center text-xl font-bold text-black">
-                We found these reportable conditions:
-              </p>
-              {conditionNames.length > 0 ? (
-                <ul className="list-disc text-xl font-bold text-black">
-                  {conditionNames.map((name) => (
-                    <li key={name}>{name}</li>
-                  ))}
-                </ul>
-              ) : null}
+    <div className="flex flex-col gap-12">
+      <h2 className="font-merriweather text-3xl font-bold text-black">
+        Test filter
+      </h2>
+      <Container color="blue">
+        <Content className="flex flex-col items-center">
+          <img className="p-3" src={InformationSvg} alt="Information icon" />
+          <div className="flex flex-col items-center gap-10">
+            <div className="flex flex-col items-center gap-6">
+              <div className="flex flex-col items-center gap-3">
+                <p className="text-center text-xl font-bold text-black">
+                  We found these reportable conditions:
+                </p>
+                {conditionNames.length > 0 ? (
+                  <ul className="list-disc text-xl font-bold text-black">
+                    {conditionNames.map((name) => (
+                      <li key={name}>{name}</li>
+                    ))}
+                  </ul>
+                ) : null}
+              </div>
+            </div>
+            <div>
+              <Button onClick={onClick}>Refine eCR</Button>
             </div>
           </div>
-          <div>
-            <Button onClick={onClick}>Refine eCR</Button>
-          </div>
-        </div>
-      </Content>
-    </Container>
+        </Content>
+      </Container>
+    </div>
   );
 }
--- client/src/pages/Demo/RunTest.tsx
+++ client/src/pages/Demo/RunTest.tsx
@@ -8,46 +8,51 @@ interface RunTestProps {
 }
 export function RunTest({ onClick }: RunTestProps) {
   return (
-    <div className="flex flex-col gap-6 md:flex-row">
-      <Container color="white">
-        <Content className="flex items-start gap-6">
-          <img className="px-3 py-1" src={UploadSvg} alt="" />
-          <div className="flex flex-col gap-10">
-            <p className="flex flex-col gap-2 text-black">
-              <span className="font-bold">
-                Want to refine your own eCR file?
-              </span>
-              <span>Please upload a single eICR/RR pair as a .zip file.</span>
-            </p>
-            <div>
-              <Button onClick={() => null}>Upload .zip file</Button>
+    <div className="flex flex-col gap-12">
+      <h2 className="font-merriweather text-3xl font-bold text-black">
+        Test filter
+      </h2>
+      <div className="flex flex-col gap-6 xl:flex-row">
+        <Container color="white">
+          <Content className="flex items-start gap-6">
+            <img className="px-3 py-1" src={UploadSvg} alt="" />
+            <div className="flex flex-col gap-10">
+              <p className="flex flex-col gap-2 text-black">
+                <span className="font-bold">
+                  Want to refine your own eCR file?
+                </span>
+                <span>Please upload a single eICR/RR pair as a .zip file.</span>
+              </p>
+              <div>
+                <Button onClick={() => null}>Upload .zip file</Button>
+              </div>
             </div>
-          </div>
-        </Content>
-      </Container>
-      <Container color="blue">
-        <Content className="flex items-start gap-6">
-          <img className="px-3 py-5" src={ForwardSvg} alt="" />
-          <div className="flex flex-col gap-10">
-            <p className="flex flex-col gap-2 text-black">
-              <span className="font-bold">Don't have a file ready?</span>
-              <span>You can try out eCR Refiner with our test file.</span>
-            </p>
-            <div className="flex flex-col items-center gap-4 md:flex-row">
-              <Button variant="secondary" onClick={onClick}>
-                Use test file
-              </Button>
-              <a
-                className="text-violet-warm-60 hover:text-violet-warm-70 justify-start font-bold hover:underline"
-                href="/api/v1/demo/download"
-                download
-              >
-                Download test file
-              </a>
+          </Content>
+        </Container>
+        <Container color="blue">
+          <Content className="flex items-start gap-6">
+            <img className="px-3 py-5" src={ForwardSvg} alt="" />
+            <div className="flex flex-col gap-10">
+              <p className="flex flex-col gap-2 text-black">
+                <span className="font-bold">Don't have a file ready?</span>
+                <span>You can try out eCR Refiner with our test file.</span>
+              </p>
+              <div className="flex flex-col items-center gap-4 md:flex-row">
+                <Button variant="secondary" onClick={onClick}>
+                  Use test file
+                </Button>
+                <a
+                  className="text-violet-warm-60 hover:text-violet-warm-70 justify-start font-bold hover:underline"
+                  href="/api/v1/demo/download"
+                  download
+                >
+                  Download test file
+                </a>
+              </div>
             </div>
-          </div>
-        </Content>
-      </Container>
+          </Content>
+        </Container>
+      </div>
     </div>
   );
 }
