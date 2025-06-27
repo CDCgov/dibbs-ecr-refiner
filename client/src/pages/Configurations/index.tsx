632cf90b9f0735be7abf7c36cf391f86a8d026c1
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2025-06-27 17:12:00 -0500

    feat(ui): Adds Configurations and Testing pages;
    This adds a Configurations page is that is now the replacement to the
    Landing page since that's been removed. This also adds a Testing page
    which is a renamed Demo page. Contents and Layouts of the Configurations
    page is still a work in progress right now.

---
+++ client/src/pages/Configurations/index.tsx
@@ -0,0 +1,11 @@
+export function Configurations() {
+  let title = `Your reportable condition configurations`
+  let subtitle = `Set up reportable configurations herer to specifiy the data you'd like to retain in the refined eCRs for that condition.`
+
+  return (
+    <>
+    <h1>{title}</h1>
+    <p>{subtitle}</p>
+    </>
+  )
+}
--- client/src/pages/Testing/index.tsx
+++ client/src/pages/Testing/index.tsx
@@ -1,4 +1,4 @@
-import { LandingPageLink } from '../../components/LandingPageLink';
+// import { LandingPageLink } from '../../components/LandingPageLink';
 import { Success } from './Success';
 import { ReportableConditions } from './ReportableConditions';
 import { Error } from './Error';
@@ -74,7 +74,6 @@ export default function Demo() {
   return (
     <div className="flex justify-center px-10 md:px-20">
       <div className="flex flex-col gap-10 py-10">
-        <LandingPageLink />
         {view === 'run-test' && (
           <RunTest
             onClickSampleFile={runTestWithSampleFile}
