0e0b24b2b59cffdfe5527d40afbcf4ad18c0b240
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2025-07-01 18:38:01 -0500

    feat(ui): Add basic test for now

---
+++ client/src/components/Search/index.test.tsx
@@ -0,0 +1,28 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router';
+import { Search } from '.';
+
+const renderComponentView = (text?: string) =>
+  render(
+    <BrowserRouter>
+      <Search placeholder={text} />
+    </BrowserRouter>
+  );
+
+describe('Search component', () => {
+  it('should have a default placeholder text', async () => {
+    renderComponentView();
+
+    expect(
+      screen.getByPlaceholderText('Search')
+    ).toBeInTheDocument();
+  });
+  it('should have a custom placeholder text', async () => {
+    renderComponentView("My custom placeholder text");
+
+    expect(
+      screen.getByPlaceholderText('My custom placeholder text')
+    ).toBeInTheDocument();
+  });
+});
