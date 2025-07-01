845877d02f685dc6d156cbaf8d21052de8784dc5
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2025-07-01 18:46:54 -0500

    test(ui): Add basic Configurations Page assertions

--- client/src/pages/Configurations/index.test.tsx
+++ client/src/pages/Configurations/index.test.tsx
@@ -1,13 +1,31 @@
-import { describe, it, assert } from 'vitest';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router';
+import { Configurations } from '.';
+
+const renderPageView = () =>
+  render(
+    <BrowserRouter>
+      <Configurations />
+    </BrowserRouter>
+  );
 
 describe('Configurations', () => {
-  it('should contain a table with certain columns', async () => {
+  it.skip('should contain a table with certain columns', async () => {
     assert.fail('NOT IMPLEMENTED');
   });
   it('should contain a call-to-action button', async () => {
-    assert.fail('NOT IMPLEMENTED');
+    renderPageView();
+    expect(
+      screen.getByText('Set up new condition', {
+        selector: 'button.usa-button[type=button]',
+      })
+    ).toBeInTheDocument();
   });
-  it('should have a search component', async () => {
-    assert.fail('NOT IMPLEMENTED');
+  it('should have a search component with correct placeholder', async () => {
+    renderPageView();
+    expect(
+      screen.getByPlaceholderText('Search configurations')
+    ).toBeInTheDocument();
   });
 });
