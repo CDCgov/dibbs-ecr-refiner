6b55234362655e559f4276ce49db1b2b19c9c43c
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2025-07-03 14:11:04 -0500

    refactor(ui): Update Page

--- client/src/pages/Configurations/index.tsx
+++ client/src/pages/Configurations/index.tsx
@@ -2,18 +2,23 @@ import { Title } from '../../components/Title';
 import { Button } from '../../components/Button';
 import { Search } from '../../components/Search';
 
-const title = `Your reportable condition configurations`;
-const subtitle = `Set up reportable configurations herer to specifiy the data you'd like to retain in the refined eCRs for that condition.`;
-
 export function Configurations() {
   return (
     <section className="mx-auto p-3">
       <div className="flex flex-col gap-4 py-10">
-        <Title>{title}</Title>
-        <p>{subtitle}</p>
+        <Title>Your reportable condition configurations</Title>
+        <p>
+          Set up reportable configurations herer to specifiy the data you'd like
+          to retain in the refined eCRs for that condition.
+        </p>
       </div>
       <div className="flex flex-col justify-between gap-10 sm:flex-row sm:items-start">
-        <Search placeholder="Search configurations" />
+        <Search
+          placeholder="Search configurations"
+          id={'search-configurations'}
+          name={'search'}
+          type={'text'}
+        />
         <div>
           <Button>Set up new condition</Button>
         </div>
