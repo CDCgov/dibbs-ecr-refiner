91cb6820c2d2c951ae65980a552bfadfe60135d0
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2025-07-03 14:21:33 -0500

    fix(build): Updates a forgotten test

--- client/src/components/Search/index.test.tsx
+++ client/src/components/Search/index.test.tsx
@@ -6,7 +6,7 @@ import { Search } from '.';
 const renderComponentView = (text?: string) =>
   render(
     <BrowserRouter>
-      <Search placeholder={text} />
+      <Search placeholder={text} id={'test'} name={'test'} type={'text'} />
     </BrowserRouter>
   );
 
