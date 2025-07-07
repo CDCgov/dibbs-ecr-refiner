108843faceaf016fbb1481fa30061b578c001259
Jake Wheeler <jake.wheeler@skylight.digital>
2025-07-07 16:29:35 -0400

    remove prints

--- refiner/tests/integration/test_ecr_refiner.py
+++ refiner/tests/integration/test_ecr_refiner.py
@@ -1,5 +1,4 @@
 import inspect
-import json
 from pathlib import Path
 
 import httpx
@@ -127,8 +126,6 @@ def test_zip_upload_mon_mothma_two_conditions(setup):  # 'setup' fixture
 
     # parse the json response from the service
     response_json = response.json()
-    print(json.dumps(response_json[0], indent=2))
-    print(f"Length of refined_eicr: {len(response_json[0]['refined_eicr'])}")
 
     # assertions on the structure and content of the json response ---
     assert isinstance(response_json, list), (
