9c2afb01ac33ad7000aa62ecc7567d3609dc3285
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2025-07-07 18:30:47 -0500

    feat(ui): Add support for skipping to content

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -11,8 +11,14 @@ interface LayoutProps {
 export function Layout({ children }: LayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
+      <a className="usa-skipnav" href="#main-content">
+        Skip to main content
+      </a>
       <Header />
-      <main className="bg-primary-container flex grow flex-col">
+      <main
+        id="main-content"
+        className="bg-primary-container flex grow flex-col"
+      >
         {children}
       </main>
       <Footer />
