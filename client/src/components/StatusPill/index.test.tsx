f219972e904258e0b4a8f7252055d7a6395ff837
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2025-07-10 10:42:55 -0500

    feat(ui): Add `<ConfigurationsTable />` component (#187)
    Co-authored-by: Jake Wheeler <jake.wheeler@skylight.digital>

---
+++ client/src/components/StatusPill/index.test.tsx
@@ -0,0 +1,24 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router';
+import { StatusPill } from '.';
+
+const renderComponentView = (status: 'on' | 'off') =>
+  render(
+    <BrowserRouter>
+      <StatusPill status={status} />
+    </BrowserRouter>
+  );
+
+describe('Status pill component', () => {
+  it('should render with status on', async () => {
+    renderComponentView('on');
+
+    expect(screen.getByText('Refiner on')).toBeInTheDocument();
+  });
+  it('should render with status off', async () => {
+    renderComponentView('off');
+
+    expect(screen.getByText('Refiner off')).toBeInTheDocument();
+  });
+});
