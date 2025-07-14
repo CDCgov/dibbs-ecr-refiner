c98ad7fb5443bb3ea9bfc900aad19739802ffa12
Jake Wheeler <jake.wheeler@skylight.digital>
2025-07-14 17:00:44 -0400

    client user type gets database user info

--- client/src/hooks/Login.tsx
+++ client/src/hooks/Login.tsx
@@ -1,24 +1,8 @@
 import { useEffect, useState } from 'react';
 
 interface User {
-  exp: number;
-  iat: number;
-  auth_time: number;
-  jti: string;
-  iss: string;
-  aud: string;
-  sub: string;
-  typ: string;
-  azp: string;
-  nonce: string;
-  sid: string;
-  at_hash: string;
-  acr: string;
-  email_verified: boolean;
-  name: string;
-  preferred_username: string;
-  given_name: string;
-  family_name: string;
+  id: string;
+  username: string;
   email: string;
 }
 
