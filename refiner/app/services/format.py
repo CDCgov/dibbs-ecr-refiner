a2b90cc3890e443a032abe6305ff65d74f7e8281
Lina Roth <lina@skylight.digital>
2025-07-21 14:56:51 -0400

    formatting issues

--- refiner/app/services/format.py
+++ refiner/app/services/format.py
@@ -1,8 +1,9 @@
+
 from lxml import etree
 from lxml.etree import _Element
-from typing import cast
 
-# mypy: ignore-errors 
+
+# mypy: ignore-errors
 def normalize_xml(xml: str) -> str:
     """
     Normalize XML string for comparison: parse, strip out all comments, then re-serialize with consistent pretty-printing.
