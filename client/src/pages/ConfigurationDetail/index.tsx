bd4816f5b57d8a4928a5f96b00e4556092a106cf
Jake Wheeler <jake.wheeler@skylight.digital>
2025-07-28 11:13:43 -0400

    continued

--- client/src/pages/ConfigurationDetail/index.tsx
+++ client/src/pages/ConfigurationDetail/index.tsx
@@ -13,7 +13,6 @@ export default function ConfigurationDetail() {
       <div className="flex flex-col gap-4 bg-white px-8 pt-8 pb-6 shadow-lg sm:flex-row sm:justify-between sm:px-20">
         <div>
           <Title>Name</Title>
-          <p>{id}</p>
         </div>
         <div className="flex items-center gap-2">
           <Button variant="secondary">Export</Button>
