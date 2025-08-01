ccc79927de18f6aefae41e157272f8d09d77c401
Jake Wheeler <jake.wheeler@skylight.digital>
2025-08-01 10:46:38 -0400

    more clean up

--- client/src/hooks/useToast.tsx
+++ client/src/hooks/useToast.tsx
@@ -1,4 +1,3 @@
-import { HeadingLevel } from '@trussworks/react-uswds';
 import classNames from 'classnames';
 import { toast, ToastOptions } from 'react-toastify';
 import { Toast } from '../components/Toast';
@@ -6,7 +5,6 @@ import { Toast } from '../components/Toast';
 const globalOptions: ToastOptions = {
   // uncomment this to debug toast styling issues
   // progress: 0.2,
-  hideProgressBar: false,
   position: 'bottom-left',
   closeOnClick: true,
   closeButton: false,
@@ -26,20 +24,12 @@ export function useToast() {
   return (options: {
     body?: string;
     heading?: string;
-    headingLevel?: HeadingLevel;
     duration?: number;
     hideProgressBar?: boolean;
   }) =>
-    toast(
-      <Toast
-        body={options.body ?? ''}
-        heading={options.heading}
-        headingLevel={options.headingLevel}
-      />,
-      {
-        ...globalOptions,
-        autoClose: options.duration ?? 5000,
-        hideProgressBar: options.hideProgressBar ?? false,
-      }
-    );
+    toast(<Toast body={options.body ?? ''} heading={options.heading ?? ''} />, {
+      ...globalOptions,
+      autoClose: options.duration ?? 5000,
+      hideProgressBar: options.hideProgressBar ?? false,
+    });
 }
