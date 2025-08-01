644ee6adb6ec73e3683aad432fceeb6b6f3c5f9b
Jake Wheeler <jake.wheeler@skylight.digital>
2025-08-01 09:50:16 -0400

    wip working

--- client/src/components/Toast/index.tsx
+++ client/src/components/Toast/index.tsx
@@ -2,24 +2,21 @@ import { HeadingLevel, Icon } from '@trussworks/react-uswds';
 import classNames from 'classnames';
 import { toast, ToastOptions } from 'react-toastify';
 
-export type AlertType = 'info' | 'success' | 'warning' | 'error';
-
 type ToastProps = {
-  toastVariant: AlertType;
   heading?: string;
-  body: string;
+  body?: string;
   headingLevel?: HeadingLevel;
   hideProgressBar?: boolean;
 };
 
 const Toast: React.FC<ToastProps> = ({ heading, body }) => {
   return (
-    <div className="usa-alert usa-alert--success w-full">
+    <div className="usa-alert usa-alert--success flex h-full w-full items-center">
       <div className="flex flex-row items-start gap-4 p-2">
         <Icon.CheckCircle aria-hidden size={4} />
         <div className="flex flex-col gap-2">
           <h4 className="font-public-sans text-xl font-bold">{heading}</h4>
-          <p className="font-public-sans">{body}</p>
+          {body ? <p className="font-public-sans">{body}</p> : null}
         </div>
       </div>
     </div>
@@ -33,30 +30,30 @@ const globalOptions: ToastOptions = {
   position: 'bottom-left',
   closeOnClick: true,
   closeButton: false,
-  className: classNames('!p-0', '!m-0'),
+  className: classNames(
+    '!p-0',
+    '!m-0',
+    '!w-[26.25rem]',
+    '!h-[4.5rem]',
+    '!bg-state-success-lighter',
+    '!items-center'
+  ),
   pauseOnFocusLoss: false,
   pauseOnHover: true,
 };
 
 export function useToast(options: {
-  body: string;
+  body?: string;
   heading?: string;
-  variant?: AlertType;
   headingLevel?: HeadingLevel;
   duration?: number;
   hideProgressBar?: boolean;
 }) {
-  const { body, variant, heading, duration, hideProgressBar, headingLevel } =
-    options;
+  const { body, heading, duration, hideProgressBar, headingLevel } = options;
 
   return () =>
     toast(
-      <Toast
-        body={body ?? ''}
-        toastVariant={variant ?? 'success'}
-        heading={heading}
-        headingLevel={headingLevel}
-      />,
+      <Toast body={body ?? ''} heading={heading} headingLevel={headingLevel} />,
       {
         ...globalOptions,
         autoClose: duration ?? 5000,
