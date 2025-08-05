ebb1df0aacdefcb8fb7bd5ae90a6325f418e7c25
Jake Wheeler <jake.wheeler@skylight.digital>
2025-08-05 19:14:54 -0400

    feat: Add configuration detail page (#226)

---
+++ client/src/pages/Configurations/ConfigActivate/index.tsx
@@ -0,0 +1,31 @@
+import { useParams } from 'react-router';
+import { Title } from '../../../components/Title';
+import NotFound from '../../NotFound';
+import {
+  NavigationContainer,
+  SectionContainer,
+  TitleContainer,
+} from '../layout';
+import { StepsContainer, Steps } from '../Steps';
+
+export default function ConfigActivate() {
+  const { id } = useParams<{ id: string }>();
+
+  if (!id) return <NotFound />;
+
+  return (
+    <div>
+      <TitleContainer>
+        <Title>Condition name</Title>
+      </TitleContainer>
+      <NavigationContainer>
+        <StepsContainer>
+          <Steps configurationId={id} />
+        </StepsContainer>
+      </NavigationContainer>
+      <SectionContainer>
+        <div>Configuration activation</div>
+      </SectionContainer>
+    </div>
+  );
+}
