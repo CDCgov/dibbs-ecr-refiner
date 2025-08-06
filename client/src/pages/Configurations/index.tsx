ab44337a3a08663be799ef7d14dbe320204c5e96
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2025-08-06 13:25:53 -0500

    feat(ui): Add Configuration (#227)
    Co-authored-by: Jake Wheeler <jake.wheeler@skylight.digital>

--- client/src/pages/Configurations/index.tsx
+++ client/src/pages/Configurations/index.tsx
@@ -4,44 +4,106 @@ import { Search } from '../../components/Search';
 import { ConfigurationsTable } from '../../components/ConfigurationsTable';
 import { useToast } from '../../hooks/useToast';
 
+import {
+  ModalToggleButton,
+  Modal,
+  ModalHeading,
+  ModalFooter,
+  ModalRef,
+  Form,
+  Label,
+  ComboBox,
+  ComboBoxRef,
+} from '@trussworks/react-uswds';
+import { SyntheticEvent, useRef, useState } from 'react';
+
 enum ConfigurationStatus {
   on = 'on',
   off = 'off',
 }
 
+interface SelectedCondition {
+  value: string;
+  label: string;
+}
+
+interface ConfigurationsData {
+  name: string;
+  status: ConfigurationStatus;
+  id: string;
+}
+
+interface ConfigurationsColumns {
+  [key: string]: string;
+}
+
+interface ConfigurationsTable {
+  columns: ConfigurationsColumns;
+  data: ConfigurationsData[];
+}
+
 export function Configurations() {
+  const [table, setTable] = useState<ConfigurationsTable>({
+    columns: { name: 'Reportable condition', status: 'Status' },
+    data: [],
+  });
+
+  const conditionData = {
+    // TODO: Figure out what this data should look like and require using real
+    // UUIDs since keys need to be unique.
+    '3d7fb83d-d664-4b82-a0fb-3f8decd307cc': 'Anaplasmosis',
+    '1be0a722-ead6-4a54-ad90-e83c139ceb3c': 'Chlamydia trachomatis infection',
+    '0c157c35-b9a2-431f-badc-9ee0ea12003f': 'Gonorrhea',
+    'f0365ece-3ec7-486a-ba73-7f5d1de64ca8': 'HIV',
+    '985fc9f8-86dc-4e12-95f1-b7457b3497ca': 'Syphilis',
+  };
+
   const showToast = useToast();
 
-  const tableData = {
-    columns: { name: 'Reportable condition', status: 'Status' },
-    data: [
-      {
-        name: 'Chlamydia trachomatis infection',
-        status: ConfigurationStatus.on,
-        id: 'chlamydia-config-id',
-      },
-      {
-        name: 'Disease caused by Enterovirus',
-        status: ConfigurationStatus.off,
-        id: 'enterovirus-config-id',
-      },
-      {
-        name: 'Human immunodeficiency virus infection (HIV)',
-        status: ConfigurationStatus.off,
-        id: 'hiv-config-id',
-      },
-      {
-        name: 'Syphilis',
-        status: ConfigurationStatus.on,
-        id: 'syphilis-config-id',
-      },
-      {
-        name: 'Viral hepatitis, type A',
-        status: ConfigurationStatus.on,
-        id: 'viral-hep-config-id',
-      },
-    ],
-  };
+  const conditionList = Object.entries(conditionData).map(([id, name]) => ({
+    value: id,
+    label: name,
+  }));
+
+  const modalRef = useRef<ModalRef>(null);
+  const listRef = useRef<ComboBoxRef>(null);
+
+  const [formValid, setFormValid] = useState<boolean>(false);
+  const [condition, setCondition] = useState<SelectedCondition | undefined>(
+    undefined
+  );
+
+  function handleSubmit(e: SyntheticEvent) {
+    e.preventDefault();
+    if (!condition) {
+      return;
+    }
+
+    const newData: ConfigurationsData = {
+      name: condition.label,
+      id: condition.value,
+      status: ConfigurationStatus.off,
+    };
+    setTable((prevTable) => ({
+      ...prevTable,
+      data: [...prevTable.data, newData],
+    }));
+
+    modalRef.current?.toggleModal();
+    listRef.current?.clearSelection();
+  }
+  function handleChange(selectedValue: string | undefined) {
+    if (selectedValue) {
+      const selectedOption = conditionList.find(
+        (option) => option.value === selectedValue
+      );
+      setCondition(selectedOption);
+      setFormValid(true);
+    } else {
+      setCondition(undefined);
+      setFormValid(false);
+    }
+  }
 
   return (
     <section className="mx-auto p-3">
@@ -59,6 +121,55 @@ export function Configurations() {
           name="search"
           type="text"
         />
+        <ModalToggleButton
+          modalRef={modalRef}
+          opener
+          className="!bg-violet-warm-60 hover:!bg-violet-warm-70 !m-0"
+        >
+          Set up new condition
+        </ModalToggleButton>
+        <Modal
+          ref={modalRef}
+          id="add-configuration-modal"
+          aria-labelledby="add-configuration-modal-heading"
+          aria-describedby="add-configuration-modal-description"
+          className="pb-5 !align-top"
+        >
+          <ModalHeading
+            id="add-configuration-modal-heading"
+            className="font-merriweather !text-3xl !leading-18 font-bold text-black"
+          >
+            Set up new condition
+          </ModalHeading>
+          <p id="add-configuration-modal-description" className="sr-only">
+            Select a reportable condition you'd like to configure.
+          </p>
+          <Form onSubmit={handleSubmit}>
+            <Label
+              htmlFor="new-condition"
+              className="!leading-6"
+              data-focus="true"
+            >
+              Condition
+            </Label>
+            <ComboBox
+              ref={listRef}
+              id="new-condition"
+              name="new-condition"
+              options={conditionList}
+              onChange={handleChange}
+            />
+            <ModalFooter className="flex justify-self-end">
+              <Button
+                type="submit"
+                variant={`${formValid ? 'primary' : 'disabled'}`}
+                disabled={!formValid}
+              >
+                Add condition
+              </Button>
+            </ModalFooter>
+          </Form>
+        </Modal>
         <Button
           className="m-0!"
           onClick={() => {
@@ -73,10 +184,10 @@ export function Configurations() {
             });
           }}
         >
-          Set up new condition
+          Test Toast
         </Button>
       </div>
-      <ConfigurationsTable columns={tableData.columns} data={tableData.data} />
+      <ConfigurationsTable columns={table.columns} data={table.data} />
     </section>
   );
 }
