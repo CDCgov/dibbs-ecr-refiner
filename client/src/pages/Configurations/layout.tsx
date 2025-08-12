dd9ca966930ed89df52dd5232f0cc5fa0b28d7d4
Jake Wheeler <jake.wheeler@skylight.digital>
2025-08-12 17:11:17 -0400

    slight improvement to config workflow on smaller displays

--- client/src/pages/Configurations/layout.tsx
+++ client/src/pages/Configurations/layout.tsx
@@ -4,18 +4,18 @@ export function NavigationContainer({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col gap-4 px-8 pt-8 pb-6 sm:justify-between md:px-20">
+    <div className="flex flex-col gap-4 px-8 pt-8 pb-6 sm:justify-between lg:px-20">
       {children}
     </div>
   );
 }
 
 export function SectionContainer({ children }: { children: React.ReactNode }) {
-  return <section className="px-8 pt-8 pb-6 md:px-20">{children}</section>;
+  return <section className="px-8 pt-8 pb-6 lg:px-20">{children}</section>;
 }
 
 export function TitleContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-white px-8 pt-8 pb-6 shadow-lg md:px-20">{children}</div>
+    <div className="bg-white px-8 pt-8 pb-6 shadow-lg lg:px-20">{children}</div>
   );
 }
