ef0349ef1f9207671187920ca1a7c73c0e9c07e0
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2025-09-12 10:49:16 -0500

    fix: Update tests and ergonomics for `just ...`

--- client/e2e/homepage.spec.ts
+++ client/e2e/homepage.spec.ts
@@ -3,7 +3,6 @@ import { test, expect } from '@playwright/test';
 test('homepage has expected title and content', async ({ page }) => {
   await page.goto('http://localhost:8081');
   await expect(page).toHaveTitle(/DIBBs eCR Refiner/);
-  await expect(page.getByRole('navigation')).toBeVisible();
 });
 
 test('should show a login button', async ({ page }) => {
