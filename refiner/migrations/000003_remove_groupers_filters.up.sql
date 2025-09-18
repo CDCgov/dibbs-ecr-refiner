adacd183a0809cd09c30e7b1c795a46baedc796f
robertmitchellv <robert.beaty.mitchell@gmail.com>
2025-09-18 09:21:31 -0700

    remove IF EXISTS since we want migrations to break loudly

--- refiner/migrations/000003_remove_groupers_filters.up.sql
+++ refiner/migrations/000003_remove_groupers_filters.up.sql
@@ -5,26 +5,26 @@
 
 -- drop legacy tables that have been replaced by the 'conditions' and 'configurations' tables
 -- Their presence was for temporary compatibility during development and is no longer needed.
-DROP TABLE IF EXISTS public.groupers;
-DROP TABLE IF EXISTS public.filters;
+DROP TABLE groupers;
+DROP TABLE filters;
 
 -- drop tables related to features that are planned but not yet implemented
 -- removing them now provides a clean slate for their future implementation,
 -- ensuring the design is not constrained by a pre-existing, unused schema
-DROP TABLE IF EXISTS public.activations;
-DROP TABLE IF EXISTS public.configuration_labels;
-DROP TABLE IF EXISTS public.labels;
+DROP TABLE activations;
+DROP TABLE configuration_labels;
+DROP TABLE labels;
 
 -- alter the 'configurations' table to remove columns that are not in use
-ALTER TABLE public.configurations
+ALTER TABLE configurations
     -- the 'family_id' column and its sequence were part of a versioning concept
     -- that is not currently being used. the current versioning is handled by the
     -- 'version' column alone within the scope of a jurisdiction and name
-    DROP COLUMN IF EXISTS family_id,
+    DROP COLUMN family_id,
 
     -- the 'cloned_from_configuration_id' column was intended for tracking
     -- the provenance of a configuration, a feature that is not implemented
-    DROP COLUMN IF EXISTS cloned_from_configuration_id;
+    DROP COLUMN cloned_from_configuration_id;
 
 -- drop the sequence associated with the removed 'family_id' column.
-DROP SEQUENCE IF EXISTS public.configuration_family_id_seq;
+DROP SEQUENCE configuration_family_id_seq;
