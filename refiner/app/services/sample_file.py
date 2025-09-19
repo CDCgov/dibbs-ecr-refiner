4214cf4557cb42c403560d96e31c80e8161025c7
Jake Wheeler <jake.wheeler@skylight.digital>
2025-09-19 15:28:22 -0400

    path check

--- refiner/app/services/sample_file.py
+++ refiner/app/services/sample_file.py
@@ -25,6 +25,15 @@ def create_sample_zip_file(sample_zip_path: Path) -> UploadFile:
     Returns:
         UploadFile: The file packaged as an UploadFile
     """
+
+    # Expected asset path
+    asset_root = get_asset_path("demo").resolve()
+
+    # Check that sample zip path is within expected asset path
+    fullpath = sample_zip_path.resolve()
+    if not str(fullpath).startswith(str(asset_root)):
+        raise ValueError("Attempt to access file outside allowed asset directory.")
+
     filename = sample_zip_path.name
     with open(sample_zip_path, "rb") as demo_file:
         zip_content = demo_file.read()
