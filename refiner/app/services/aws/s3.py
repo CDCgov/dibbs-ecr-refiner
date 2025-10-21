5e1685a6aa89efbdc5b1637f46ac0b73430cd2e5
robertmitchellv <robert.beaty.mitchell@gmail.com>
2025-10-20 22:27:44 -0700

    create an internal env var for the server-to-server communication

--- refiner/app/services/aws/s3.py
+++ refiner/app/services/aws/s3.py
@@ -19,7 +19,7 @@ s3_client = boto3.client(
     region_name=ENVIRONMENT["AWS_REGION"],
     aws_access_key_id=ENVIRONMENT["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key=ENVIRONMENT["AWS_SECRET_ACCESS_KEY"],
-    endpoint_url=os.getenv("S3_ENDPOINT_URL"),
+    endpoint_url=os.getenv("S3_ENDPOINT_URL_INTERNAL"),
     config=config,
 )
 
