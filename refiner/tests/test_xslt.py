5f3b9d822027841cca7ef998697a7ebad0d6cdff
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2025-10-24 13:30:16 -0400

    fix: Remove unused test

--- refiner/tests/test_xslt.py
+++ refiner/tests/test_xslt.py
@@ -1,7 +1,5 @@
 import logging
-import os
 import tempfile
-from pathlib import Path
 
 import pytest
 
@@ -48,29 +46,3 @@ def test_transform_xml_to_html_malformed_xslt() -> None:
             transform_xml_to_html(
                 VALID_CDA_XML, xslt_file.name, logging.getLogger("xslt-test")
             )
-
-
-def test_transform_xml_to_html_xslt_outside_trusted_directory() -> None:
-    """
-    Should raise XSLTTransformationError if xslt file is outside trusted directory.
-    """
-    VALID_CDA_XML = b"""
-    <ClinicalDocument xmlns="urn:hl7-org:v3">
-      <recordTarget></recordTarget>
-    </ClinicalDocument>
-    """
-    with tempfile.NamedTemporaryFile(
-        suffix=".xslt", dir=tempfile.gettempdir(), delete=False
-    ) as xslt_file:
-        xslt_file.write(MINIMAL_XSLT)
-        xslt_file.flush()
-        xslt_file_path = Path(xslt_file.name)
-    try:
-        # This file is definitely not under the assets/xslt trusted dir.
-        with pytest.raises(XSLTTransformationError) as exc_info:
-            transform_xml_to_html(
-                VALID_CDA_XML, xslt_file_path, logging.getLogger("xslt-test")
-            )
-        assert "outside trusted directory" in str(exc_info.value)
-    finally:
-        os.unlink(xslt_file_path)
