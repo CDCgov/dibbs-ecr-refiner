8587f81fafc244d647c044b67e66b761f866e8da
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2025-11-07 16:01:56 -0600

    fix(ui): Place the Drawer above the Toasts

--- client/src/components/Drawer/index.tsx
+++ client/src/components/Drawer/index.tsx
@@ -65,7 +65,7 @@ const Drawer = ({
       <div>
         <div
           className={classNames(
-            'bg-gray-2 fixed top-0 z-[1050] flex h-full w-full shrink-0 flex-col items-start gap-6 border-l border-solid border-gray-400 p-0 shadow-2xl transition-all duration-300 ease-linear',
+            'bg-gray-2 fixed top-0 z-[11049] flex h-full w-full shrink-0 flex-col items-start gap-6 border-l border-solid border-gray-400 p-0 shadow-2xl transition-all duration-300 ease-linear',
             {
               'pointer-events-auto right-0 opacity-100': isOpen,
               'pointer-events-none right-[-60%] opacity-0': !isOpen,
@@ -113,7 +113,7 @@ const Drawer = ({
 
         {isOpen && (
           <div
-            className="fixed top-0 left-0 z-[1040] h-full w-full"
+            className="fixed top-0 left-0 z-[11039] h-full w-full"
             onClick={handleClose}
           ></div>
         )}
