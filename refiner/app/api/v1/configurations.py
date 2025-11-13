d0f20d05f9c732d49ead7747f2a5ccd0a65849c1
Jake Wheeler <jake.wheeler@skylight.digital>
2025-11-13 16:29:31 -0500

    move to services

--- refiner/app/api/v1/configurations.py
+++ refiner/app/api/v1/configurations.py
@@ -27,6 +27,9 @@ from app.core.exceptions import (
     XMLValidationError,
     ZipValidationError,
 )
+from app.services.configurations import (
+    get_canonical_url_to_highest_inactive_version_map,
+)
 from app.services.ecr.refine import get_file_size_reduction_percentage
 from app.services.file_io import (
     create_refined_ecr_zip_in_memory,
@@ -78,32 +81,6 @@ from ...services.xslt import (
 )
 
 router = APIRouter(prefix="/configurations")
-
-
-def get_canonical_url_to_highest_inactive_version_map(
-    configs: list[DbConfiguration],
-) -> dict[str, DbConfiguration]:
-    """
-    Creates a dictionary that maps a condition URL to the highest inactive version configuration.
-
-    Args:
-        configs (list[DbConfiguration]): List of DbConfigurations
-
-    Returns:
-    a dictionary with the structure:
-        key = Condition canonical URL
-        value = Inactive configuration with highest version number
-    """
-    highest_version_inactive_configs_map: dict[str, DbConfiguration] = {}
-    for c in configs:
-        if c.status == "inactive":
-            key = c.condition_canonical_url
-            if (
-                key not in highest_version_inactive_configs_map
-                or c.version > highest_version_inactive_configs_map[key].version
-            ):
-                highest_version_inactive_configs_map[key] = c
-    return highest_version_inactive_configs_map
 
 
 @dataclass(frozen=True)
