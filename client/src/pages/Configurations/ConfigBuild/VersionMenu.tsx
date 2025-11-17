5a59f271e6478232d8123797dd696df3ccc93e62
Jake Wheeler <jake.wheeler@skylight.digital>
2025-11-17 18:54:34 -0500

    menu

--- client/src/pages/Configurations/ConfigBuild/VersionMenu.tsx
+++ client/src/pages/Configurations/ConfigBuild/VersionMenu.tsx
@@ -21,33 +21,43 @@ export function VersionMenu({
   step,
 }: VersionMenuProps) {
   return (
-    <Menu as="div" className="z-10">
-      <MenuButton>
-        <div className="cursor-pointer">
-          <span className="font-bold">
-            {status === 'draft' ? 'Editing' : 'Viewing'}: Version{' '}
-            {currentVersion}
-          </span>
-          <Icon.ArrowDropDown aria-hidden />
-        </div>
-      </MenuButton>
-      <MenuItems className="absolute mt-1 ml-4 flex flex-col rounded-lg bg-white px-4 py-2 shadow-lg">
-        {versions.map((v, i) => (
-          <>
-            <MenuItem key={v.id}>
-              <Link
-                className="data-focus:bg-gray-10 block p-4 font-bold"
-                to={`/configurations/${v.id}/${step}`}
-              >
-                Version {v.version}
-              </Link>
-            </MenuItem>
-            {versions.length - 1 !== i && (
-              <div aria-hidden className="bg-gray-cool-10 my-1 h-px" />
-            )}
-          </>
-        ))}
-      </MenuItems>
-    </Menu>
+    <>
+      <Menu as="div" className="z-10">
+        <MenuButton>
+          <div className="cursor-pointer">
+            <span className="font-bold">
+              {status === 'draft' ? 'Editing' : 'Viewing'}: Version{' '}
+              {currentVersion}
+            </span>
+            <Icon.ArrowDropDown aria-hidden />
+          </div>
+        </MenuButton>
+        <MenuItems className="absolute mt-1 ml-4 flex flex-col rounded-lg bg-white px-4 py-2 shadow-lg">
+          {versions.map((v, i) => (
+            <>
+              <MenuItem key={v.id}>
+                <Link
+                  className="data-focus:bg-gray-10 block p-4 font-bold"
+                  to={`/configurations/${v.id}/${step}`}
+                >
+                  Version {v.version}
+                </Link>
+              </MenuItem>
+              {versions.length - 1 !== i && (
+                <div
+                  aria-hidden
+                  key={`${v.id}-divider`}
+                  className="bg-gray-cool-10 my-1 h-px"
+                ></div>
+              )}
+            </>
+          ))}
+        </MenuItems>
+      </Menu>
+      <div
+        aria-hidden
+        className="border-gray-cool-20 mx-1 hidden h-10 border-l md:block"
+      ></div>
+    </>
   );
 }
