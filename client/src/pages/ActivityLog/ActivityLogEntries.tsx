ec896e31e5ad58931180beae584a8a1369009a8b
Bob Zhao <bob@skylight.digital>
2025-11-17 14:54:05 -0500

    hook up filtering

---
+++ client/src/pages/ActivityLog/ActivityLogEntries.tsx
@@ -0,0 +1,60 @@
+import { EventResponse } from '../../api/schemas';
+import Table from '../../components/Table';
+
+const timeFormatter = new Intl.DateTimeFormat('en-US', {
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: true,
+});
+interface ActivityLogEntriesProps {
+  displayResponses: EventResponse[];
+}
+
+export function ActivityLogEntries({
+  displayResponses,
+}: ActivityLogEntriesProps) {
+  const nameHeader = 'Name';
+  const conditionHeader = 'Condition';
+  const actionHeader = 'Action';
+  const dateHeader = 'Date';
+
+  return (
+    <Table striped>
+      <colgroup>
+        <col className="w-full sm:w-1/6" />
+        <col className="w-2/6" />
+        <col className="w-2/6" />
+        <col className="w-1/6" />
+      </colgroup>
+      <thead>
+        <tr>
+          <th scope="col">{nameHeader} </th>
+          <th scope="col">{conditionHeader} </th>
+          <th scope="col">{actionHeader}</th>
+          <th scope="col">{dateHeader}</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {displayResponses
+          .sort((a, b) => (a.created_at > b.created_at ? -1 : 1))
+          .map((r) => {
+            const createdAtDate = new Date(r.created_at);
+            return (
+              <tr key={r.id}>
+                <td data-label={nameHeader} className="!font-bold">
+                  {r.username}
+                </td>
+                <td data-label={conditionHeader}>{r.configuration_name}</td>
+                <td data-label={actionHeader}>{r.action_text}</td>
+                <td data-label={dateHeader}>
+                  {createdAtDate.toLocaleDateString()} <br />
+                  {timeFormatter.format(createdAtDate)}
+                </td>
+              </tr>
+            );
+          })}
+      </tbody>
+    </Table>
+  );
+}
