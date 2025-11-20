bd8cae37d67834cc29587728009207d16b934bca
Jake Wheeler <jake.wheeler@skylight.digital>
2025-11-20 11:45:30 -0500

    more complete comments for activation page

--- client/src/pages/Configurations/ConfigActivate/index.tsx
+++ client/src/pages/Configurations/ConfigActivate/index.tsx
@@ -8,10 +8,26 @@ import {
 } from '../layout';
 import { StepsContainer, Steps } from '../Steps';
 import { ConfigurationTitleBar } from '../ConfigurationTitleBar';
+// import { useGetConfiguration } from '../../../api/configurations/configurations';
+// import { Spinner } from '../../../components/Spinner';
+// import ErrorFallback from '../../ErrorFallback';
 
 export default function ConfigActivate() {
   const { id } = useParams<{ id: string }>();
 
+  // const {
+  //   data: response,
+  //   isPending,
+  //   isError,
+  //   error,
+  // } = useGetConfiguration(id ?? '');
+
+  // if (isPending) return <Spinner variant="centered" />;
+  // if (!id || isError) return <ErrorFallback error={error} />;
+
+  /**
+   * Delete this and uncomment everything else.
+   */
   if (!id) return <NotFound />;
 
   return (
@@ -26,7 +42,7 @@ export default function ConfigActivate() {
           currentVersion={response.data.version}
           status={response.data.status}
           versions={response.data.all_versions}
-          step="test"
+          step="activate"
         /> */}
         <StepsContainer>
           <Steps configurationId={id} />
