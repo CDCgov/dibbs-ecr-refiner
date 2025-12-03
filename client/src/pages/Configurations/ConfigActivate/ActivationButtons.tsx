e24927163c0396a54aaf2fdf772ed2f698f3f8e4
Bob Zhao <bob@skylight.digital>
2025-12-03 09:25:56 -0600

    activation frontendup

--- client/src/pages/Configurations/ConfigActivate/ActivationButtons.tsx
+++ client/src/pages/Configurations/ConfigActivate/ActivationButtons.tsx
@@ -14,6 +14,12 @@ import {
 } from '@trussworks/react-uswds';
 import classNames from 'classnames';
 
+enum ModalStates {
+  TURN_ON,
+  TURN_OFF,
+  SWITCH_FROM_PREVIOUS,
+}
+
 const EMPTY_BUTTON_STATE = {
   activate: false,
   deactivate: false,
@@ -21,7 +27,7 @@ const EMPTY_BUTTON_STATE = {
   deactivateText: '',
   deactivateHelper: '',
   activateHelper: '',
-  switchFromPrevious: false,
+  modalKey: ModalStates.TURN_OFF,
 };
 
 interface ActivationButtonsProps {
@@ -54,9 +60,11 @@ export function ActivationButtons({
     if (currentVersionIsActive) {
       newButtonState['deactivate'] = true;
       newButtonState['deactivateText'] = 'Turn off configuration';
+      newButtonState['modalKey'] = ModalStates.TURN_OFF;
     } else if (noCurrentActiveConfig) {
       newButtonState['activate'] = true;
       newButtonState['activateText'] = 'Turn on configuration';
+      newButtonState['modalKey'] = ModalStates.TURN_ON;
     } else {
       newButtonState['activate'] = true;
       newButtonState['activateText'] =
@@ -64,26 +72,27 @@ export function ActivationButtons({
       newButtonState['deactivate'] = true;
       newButtonState['deactivateText'] = 'Turn off current version';
       newButtonState['activateHelper'] =
-        'Safely replace the current version with this one -- it will begin processing immediately';
+        'Safely replace the current version with this one — it will begin processing immediately';
       newButtonState['deactivateHelper'] =
         'Stop the current version. No version will be active until you turn one on';
-      newButtonState['switchFromPrevious'] = true;
+      newButtonState['modalKey'] = ModalStates.SWITCH_FROM_PREVIOUS;
     }
 
     setButtonState(newButtonState);
   }, [isSuccess, configurationData]);
 
-  const modalRef = useRef<ModalRef>(null);
+  const openRef = useRef<ModalRef>(null);
+  const closeRef = useRef<ModalRef>(null);
 
   return (
     <div className="mt-6">
-      {buttonState['switchFromPrevious'] && (
+      {buttonState['modalKey'] === ModalStates.SWITCH_FROM_PREVIOUS && (
         <h3 className="mb-4 text-lg font-bold">Option 1</h3>
       )}
       {buttonState['activate'] && (
         <div>
           <ModalToggleButton
-            modalRef={modalRef}
+            modalRef={openRef}
             opener
             className={classNames('self-start', PRIMARY_BUTTON_STYLES)}
           >
@@ -92,23 +101,37 @@ export function ActivationButtons({
           <>{buttonState['activateHelper']}</>
 
           <ActivationConfirmationModal
-            modalRef={modalRef}
+            modalRef={openRef}
             handleActivation={handleActivation}
-            switchFromPrevious={buttonState['switchFromPrevious']}
+            handleDeactivation={handleDeactivation}
+            modalKey={buttonState['modalKey']}
             curVersion={curVersion}
             activeVersion={activeVersion}
           />
         </div>
       )}
-      {buttonState['switchFromPrevious'] && (
+      {buttonState['modalKey'] === ModalStates.SWITCH_FROM_PREVIOUS && (
         <h3 className="mt-6 mb-4 text-lg font-bold">Option 2</h3>
       )}
       {buttonState['deactivate'] && (
         <div>
-          <Button variant="secondary" onClick={handleDeactivation}>
+          <ModalToggleButton
+            modalRef={closeRef}
+            opener
+            className={classNames('self-start', SECONDARY_BUTTON_STYLES)}
+          >
             {buttonState['deactivateText']}
-          </Button>
-          {<>{buttonState['deactivateHelper']}</>}
+          </ModalToggleButton>
+          <>{buttonState['deactivateHelper']}</>
+
+          <ActivationConfirmationModal
+            modalRef={closeRef}
+            handleActivation={handleActivation}
+            handleDeactivation={handleDeactivation}
+            modalKey={ModalStates.TURN_OFF}
+            curVersion={curVersion}
+            activeVersion={activeVersion}
+          />
         </div>
       )}
     </div>
@@ -118,74 +141,107 @@ export function ActivationButtons({
 interface ActivationConfirmationModalProps {
   modalRef: RefObject<ModalRef | null>;
   handleActivation: () => void;
-  switchFromPrevious: boolean;
+  handleDeactivation: () => void;
+  modalKey: ModalStates;
   curVersion: number;
   activeVersion: number | null;
 }
 function ActivationConfirmationModal({
   modalRef,
   handleActivation,
-  switchFromPrevious,
+  handleDeactivation,
+  modalKey,
   curVersion,
   activeVersion,
 }: ActivationConfirmationModalProps) {
-  const modalContent = switchFromPrevious
-    ? {
-        title: `Switch to Version ${curVersion}`,
-        body: (
-          <div>
-            <p id="activation-confirmation-modal-text" className="my-6">
-              You're about to stop Version {activeVersion} and start Version
-              {curVersion}
-            </p>
-            <p>
-              The eCR pipeline will begin using Version {curVersion}
-              <b>immediately</b>
-            </p>
-            <p>Do you want to continue?</p>
-          </div>
-        ),
-        footer: (
-          <div>
-            <ModalToggleButton
-              modalRef={modalRef}
-              closer
-              className={SECONDARY_BUTTON_STYLES}
-            >
-              Cancel
-            </ModalToggleButton>
-            <Button onClick={() => handleActivation()}>
-              Yes, switch to Version {curVersion}
-            </Button>
-          </div>
-        ),
-      }
-    : {
-        title: 'Turn on configuration?',
-        body: (
-          <div>
-            <ul>
-              <li>
-                Refiner will <span className="text-bold">immediately</span>{' '}
-                start to refine the eCR's
-              </li>
-              <li>
-                You <span className="text-bold">cannot</span> edit this version
-                after you activate it
-              </li>
-            </ul>
-            <p id="activation-confirmation-modal-text" className="my-6">
-              Are you sure you want to turn on the configuration?
-            </p>
-          </div>
-        ),
-        footer: (
+  const modalContent: {
+    [k in ModalStates]: {
+      title: string;
+      body: React.ReactElement;
+      footer: React.ReactElement;
+    };
+  } = {
+    [ModalStates.SWITCH_FROM_PREVIOUS]: {
+      title: `Switch to Version ${curVersion}`,
+      body: (
+        <div>
+          <p id="activation-confirmation-modal-text" className="my-6">
+            You're about to stop Version {activeVersion} and start Version{' '}
+            {curVersion}
+          </p>
+          <p>
+            The eCR pipeline will begin using Version {curVersion}{' '}
+            <b>immediately</b>
+          </p>
+          <p>Do you want to continue?</p>
+        </div>
+      ),
+      footer: (
+        <div>
+          <ModalToggleButton
+            modalRef={modalRef}
+            closer
+            className={SECONDARY_BUTTON_STYLES}
+          >
+            Cancel
+          </ModalToggleButton>
           <Button onClick={() => handleActivation()}>
-            Yes, turn on configuration
+            Yes, switch to Version {curVersion}
           </Button>
-        ),
-      };
+        </div>
+      ),
+    },
+    [ModalStates.TURN_OFF]: {
+      title: `Turn off current version`,
+      body: (
+        <div>
+          <p id="deactivation-confirmation-modal-text" className="my-6">
+            You’re about to stop the current version. No versions will be
+            running until you turn on a new one. Do you want to continue?
+          </p>
+        </div>
+      ),
+      footer: (
+        <div>
+          <ModalToggleButton
+            modalRef={modalRef}
+            closer
+            className={SECONDARY_BUTTON_STYLES}
+          >
+            Cancel
+          </ModalToggleButton>
+          <Button onClick={() => handleDeactivation()}>Yes, turn off</Button>
+        </div>
+      ),
+    },
+    [ModalStates.TURN_ON]: {
+      title: 'Turn on configuration?',
+      body: (
+        <div>
+          <ul>
+            <li>
+              Refiner will <span className="text-bold">immediately</span> start
+              to refine the eCR's
+            </li>
+            <li>
+              You <span className="text-bold">cannot</span> edit this version
+              after you activate it
+            </li>
+          </ul>
+          <p id="activation-confirmation-modal-text" className="my-6">
+            Are you sure you want to turn on the configuration?
+          </p>
+        </div>
+      ),
+      footer: (
+        <Button onClick={() => handleActivation()}>
+          Yes, turn on configuration
+        </Button>
+      ),
+    },
+  };
 
+  const modalToDesplay = modalContent[modalKey];
   return (
     <Modal
       id="activation-confirmation-modal"
@@ -195,11 +251,11 @@ function ActivationConfirmationModal({
       aria-describedby="activation-confirmation-modal-text"
     >
       <ModalHeading id="activation-confirmation-modal-heading">
-        <div className="mb-6">{modalContent.title}</div>
+        <div className="mb-6">{modalToDesplay.title}</div>
       </ModalHeading>
-      {modalContent.body}
+      {modalToDesplay.body}
       <ModalFooter className="flex justify-end">
-        {modalContent.footer}
+        {modalToDesplay.footer}
       </ModalFooter>
     </Modal>
   );
