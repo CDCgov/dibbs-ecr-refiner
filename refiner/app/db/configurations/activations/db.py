c1ae495109efc6f8bd272539307e520a966ca997
Bob Zhao <bob@skylight.digital>
2025-12-05 13:55:29 -0600

    redo the activation interfaces

--- refiner/app/db/configurations/activations/db.py
+++ refiner/app/db/configurations/activations/db.py
@@ -1,52 +1,23 @@
-from typing import overload
 from uuid import UUID
 
+from fastapi import HTTPException, status
 from psycopg import AsyncCursor
 from psycopg.rows import class_row
 
 from app.db.configurations.db import (
     GetConfigurationResponseVersion,
+    get_active_config_db,
+    get_configuration_by_id_db,
 )
 from app.db.pool import AsyncDatabaseConnection
 
 
-@overload
-async def activate_configuration_db(
-    configuration_id: UUID, *, db: AsyncDatabaseConnection, cur: None = None
-) -> GetConfigurationResponseVersion | None: ...
-
-
-@overload
-async def activate_configuration_db(
+async def _activate_configuration_db(
     configuration_id: UUID,
     *,
-    db: None = None,
     cur: AsyncCursor[GetConfigurationResponseVersion],
-) -> GetConfigurationResponseVersion | None: ...
-
-
-async def activate_configuration_db(
-    configuration_id: UUID,
-    db: AsyncDatabaseConnection | None = None,
-    cur: AsyncCursor[GetConfigurationResponseVersion] | None = None,
 ) -> GetConfigurationResponseVersion | None:
-    """
-    Activate the specified configuration and return relevant status info.
-    """
-    if db is None and cur is None:
-        raise ValueError("Provide either a db connection or a cur")
-    elif db is not None:
-        async with db.get_connection() as conn:
-            async with conn.cursor(
-                row_factory=class_row(GetConfigurationResponseVersion)
-            ) as internal_cur:
-                return await activate_configuration_db(
-                    configuration_id, cur=internal_cur
-                )
-    else:
-        # this is here so mypy doesn't complain, even though it's logically guarenteed
-        assert cur is not None
-        query = """
+    query = """
             UPDATE configurations
             SET status = 'active'
             WHERE id = %s
@@ -57,61 +28,27 @@ async def activate_configuration_db(
                 condition_canonical_url;
         """
 
-        params = (configuration_id,)
-        await cur.execute(query, params)
-        row = await cur.fetchone()
+    params = (configuration_id,)
+    await cur.execute(query, params)
+    row = await cur.fetchone()
 
-        if not row:
-            return None
+    if not row:
+        return None
 
-        return GetConfigurationResponseVersion(
-            id=row.id,
-            version=row.version,
-            status=row.status,
-            condition_canonical_url=row.condition_canonical_url,
-        )
-
-
-@overload
-async def deactivate_configuration_db(
-    configuration_id: UUID, *, db: AsyncDatabaseConnection, cur: None = None
-) -> GetConfigurationResponseVersion | None: ...
+    return GetConfigurationResponseVersion(
+        id=row.id,
+        version=row.version,
+        status=row.status,
+        condition_canonical_url=row.condition_canonical_url,
+    )
 
 
-@overload
-async def deactivate_configuration_db(
+async def _deactivate_configuration_db(
     configuration_id: UUID,
     *,
-    db: None = None,
     cur: AsyncCursor[GetConfigurationResponseVersion],
-) -> GetConfigurationResponseVersion | None: ...
-
-
-async def deactivate_configuration_db(
-    configuration_id: UUID,
-    db: AsyncDatabaseConnection | None = None,
-    cur: AsyncCursor[GetConfigurationResponseVersion] | None = None,
 ) -> GetConfigurationResponseVersion | None:
-    """
-    Deactivate the specified configuration and return relevant status info.
-    """
-
-    if db is None and cur is None:
-        raise ValueError("Provide either a db connection or a cur")
-
-    elif db is not None:
-        async with db.get_connection() as conn:
-            async with conn.cursor(
-                row_factory=class_row(GetConfigurationResponseVersion)
-            ) as internal_cur:
-                return await deactivate_configuration_db(
-                    configuration_id, cur=internal_cur
-                )
-
-    else:
-        # this is here so mypy doesn't complain, even though it's logically guarenteed
-        assert cur is not None
-        query = """
+    query = """
             WITH updated AS (
                 UPDATE configurations
                 SET status = 'inactive'
@@ -128,16 +65,100 @@ async def deactivate_configuration_db(
             AND NOT EXISTS (SELECT 1 FROM updated);
         """
 
-        params = (configuration_id, configuration_id)
-        await cur.execute(query, params)
-        row = await cur.fetchone()
+    params = (configuration_id, configuration_id)
+    await cur.execute(query, params)
+    row = await cur.fetchone()
 
-        if not row:
-            return None
+    if not row:
+        return None
 
-        return GetConfigurationResponseVersion(
-            id=row.id,
-            version=row.version,
-            status=row.status,
-            condition_canonical_url=row.condition_canonical_url,
+    return GetConfigurationResponseVersion(
+        id=row.id,
+        version=row.version,
+        status=row.status,
+        condition_canonical_url=row.condition_canonical_url,
+    )
+
+
+async def activate_configuration_db(
+    configuration_id: UUID,
+    user_jurisdiction_id: str,
+    canonical_url: str,
+    db: AsyncDatabaseConnection,
+) -> GetConfigurationResponseVersion | None:
+    """
+    Activate the specified configuration and return relevant status info.
+    """
+
+    config_to_activate = await get_configuration_by_id_db(
+        id=configuration_id,
+        jurisdiction_id=user_jurisdiction_id,
+        db=db,
+    )
+    if not config_to_activate:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Configuration is not found or isn't editable by the specified user jurisdiction permissions.",
         )
+
+    current_active_config = await get_active_config_db(
+        jurisdiction_id=user_jurisdiction_id,
+        condition_canonical_url=canonical_url,
+        db=db,
+    )
+
+    async with db.get_connection() as conn:
+        async with conn.cursor(
+            row_factory=class_row(GetConfigurationResponseVersion)
+        ) as cur:
+            if not current_active_config:
+                # just activate the configuration without deactivating
+                return await _activate_configuration_db(configuration_id, cur=cur)
+            else:
+                # set up the deactivation and activation in a transaction
+                try:
+                    # perform deactivation and activation in a single transaction so we don't run into half-deactivation states
+                    deactivated_config = await _deactivate_configuration_db(
+                        configuration_id=current_active_config.id, cur=cur
+                    )
+                    if not deactivated_config:
+                        raise HTTPException(
+                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="Couldn't deactivate configuration that needed to be deactivated before activating new configuration.",
+                        )
+
+                    active_config = await _activate_configuration_db(
+                        configuration_id=config_to_activate.id, cur=cur
+                    )
+                    if not active_config:
+                        raise HTTPException(
+                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="Configuration can't be activated.",
+                        )
+                    await conn.commit()
+                    return active_config
+                except Exception as e:
+                    await conn.rollback()
+
+                    # bubble up exception to the API layer to return to the client
+                    raise e
+                finally:
+                    await cur.close()
+                    await conn.close()
+
+
+async def deactivate_configuration_db(
+    configuration_id: UUID,
+    db: AsyncDatabaseConnection,
+) -> GetConfigurationResponseVersion | None:
+    """
+    Deactivate the specified configuration and return relevant status info.
+    """
+
+    async with db.get_connection() as conn:
+        async with conn.cursor(
+            row_factory=class_row(GetConfigurationResponseVersion)
+        ) as internal_cur:
+            return await _deactivate_configuration_db(
+                configuration_id, cur=internal_cur
+            )
