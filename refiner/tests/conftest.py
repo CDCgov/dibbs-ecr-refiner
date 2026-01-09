2af8a501d3757c62eddcec48c581885989006541
Bob Zhao <bob@skylight.digital>
2026-01-09 14:46:05 -0600

    fix test envs

--- refiner/tests/conftest.py
+++ refiner/tests/conftest.py
@@ -10,9 +10,9 @@ from lxml.etree import _Element
 from app.core.models.types import XMLFiles
 from tests.fixtures.loader import load_fixture_str, load_fixture_xml
 
-os.environ["ENV"] = "mock-env"
+os.environ["ENV"] = "local"
 os.environ["DB_URL"] = "postgresql://mock@fakedb:5432/refiner"
-os.environ["PASSWORD"] = "mock"
+os.environ["DB_PASSWORD"] = "mock"
 os.environ["AUTH_PROVIDER"] = "mock-oauth-provider"
 os.environ["AUTH_CLIENT_ID"] = "mock-refiner-client"
 os.environ["AUTH_CLIENT_SECRET"] = "mock-secret"
@@ -21,7 +21,8 @@ os.environ["SESSION_SECRET_KEY"] = "mock-session-secret"
 
 os.environ["AWS_REGION"] = "us-east-1"
 os.environ["S3_ENDPOINT_URL"] = "http://localhost:4566"
-os.environ["S3_UPLOADED_FILES_BUCKET_NAME"] = "mock-bucket"
+os.environ["S3_BUCKET_CONFIG"] = "mock-bucket"
+os.environ["LOG_LEVEL"] = "debug"
 
 
 type NamespaceMap = dict[str, str]
