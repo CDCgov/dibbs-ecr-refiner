50a3b581601f2270b1e274efdfc0ef4a1314bd01
Jake Wheeler <jake.wheeler@skylight.digital>
2026-03-03 10:07:43 -0500

    chore: swap from golang-migrate to dbmate (#893)

--- refiner/migrations/000006_configs_add_created_by.down.sql
+++
@@ -1,7 +0,0 @@
--- drop foreign key constraint
-ALTER TABLE configurations
-DROP CONSTRAINT IF EXISTS configurations_created_by_fkey;
-
--- drop the column
-ALTER TABLE configurations
-DROP COLUMN IF EXISTS created_by;
--- refiner/migrations/000006_configs_add_created_by.up.sql
+++
@@ -1,8 +0,0 @@
--- add new column
-ALTER TABLE configurations
-ADD COLUMN created_by UUID NOT NULL;
-
--- add fk reference
-ALTER TABLE configurations
-ADD CONSTRAINT configurations_created_by_fkey
-  FOREIGN KEY (created_by) REFERENCES users (id);
