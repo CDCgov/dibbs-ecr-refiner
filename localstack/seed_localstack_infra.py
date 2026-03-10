09054416c88b740544965866c4ab37cf6ae0fa81
Bob Zhao <bob@skylight.digital>
2026-03-10 10:22:35 -0400

    remove extra file

--- localstack/seed_localstack_infra.py
+++
@@ -1,7 +0,0 @@
-from refiner.tests.integration.test_lambda import (
-    configure_default_setup,
-    setup_s3_client,
-)
-
-test_client = setup_s3_client()
-configure_default_setup(test_client, auto_teardown_resources=False)
