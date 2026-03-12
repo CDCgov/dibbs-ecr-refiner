6f2c0cd5361a05653ddfee2bbc4dd9e9d8e60537
Bob Zhao <bob@skylight.digital>
2026-03-12 10:58:20 -0400

    add new files

---
+++ refiner/app/lambda/README.md
@@ -0,0 +1,72 @@
+# DIBBs eCR Refiner - Lambda
+
+This package is exclusively used to build a version of the Refiner intended to run as an AWS Lambda function.
+
+## Running the eCR Refiner Lambda in production
+
+### Docker image
+
+Docker images for the Lambda can be found in the [project's image registry](https://github.com/CDCgov/dibbs-ecr-refiner/pkgs/container/dibbs-ecr-refiner%2Flambda).
+
+These images are built based on [Dockerfile.lambda](../../../Dockerfile.lambda).
+
+### Environment variables
+
+The Lambda accepts the following environment variables, some of which are required.
+
+| Name | Description | Required | Default value |
+| --- | --- | --- | --- |
+| `S3_BUCKET_CONFIG` | S3 directory containing jurisdiction configuration files | Yes | N/A |
+| `S3_ENDPOINT_URL` | Endpoint to use when configuring the S3 client. Primarily used for testing purposes and should not need to be set in production | No | N/A |
+| `EICR_INPUT_PREFIX` | S3 directory containing eICR files | No | `eCRMessageV2/` |
+| `REFINER_INPUT_PREFIX` | S3 directory containing RR files | No | `RefinerInput/` |
+| `REFINER_OUTPUT_PREFIX` | S3 directory where refined files are written | No | `RefinerOutput/` |
+| `REFINER_COMPLETE_PREFIX` | S3 directory where a completion file is written by the Refiner to indicate success | No | `RefinerComplete/` |
+
+## File structure and build
+
+You'll notice that this code is packaged in a way where `app` is the top-level and other required modules are siblings to the `lambda` directory. The reason for this is because we want to be able to import core Refiner functionality into the Lambda module in the same way that we do for the FastAPI version of the Refiner, which this structure mirrors exactly.
+
+This Docker image, along with [Dockerfile.app](/Dockerfile.app), is built, tagged, and pushed automatically when a branch is merged into `main` as part of the [Build and push Refiner image to GHCR](/.github/workflows/docker-image-push.yml) job.
+
+## Building the Docker image locally
+
+The Docker image can be built with the following command:
+
+```sh
+docker compose build --no-cache lambda
+```
+
+## Running the Docker container locally
+
+The Lambda Docker container starts up as part of the project's [Docker Compose file](../../../docker-compose.yaml). When the container is running it will accept HTTP requests at the following endpoint:
+
+`http://localhost:9000/2015-03-31/functions/function/invocations`
+
+This endpoint can be requested using an HTTP client of your choice in order to invoke the Lambda function. Note that the Lambda function expects an SQS-style JSON event as part of the request. Please refer to the [Lambda integration tests](../../tests/integration/test_lambda.py) for an example event.
+
+## Seeding Localstack for manual testing
+
+A script accessible via `just server seed-localstack` is available to put Localstack in a state suitable for local testing (ie hitting the `localhost:9000` endpoint with a POST command simulating an SQS event). The same code is used by Pytest to set up our integration tests.
+
+After containers are spun up, run `just server seed-localstack` to seed Localstack accordingly. If all goes well, you should see an example POST body that you can use to further invoke / manually test the Lambda.
+
+```json
+Seeding complete. Bucket: local-config-bucket
+Sample SQS Event to trigger Lambda:
+{
+  "Records": [
+    {
+      "messageId": "72c00f4d-ab8a-45ad-b312-b40cba7bf70d",
+      "receiptHandle": "3eb200c6-9291-4074-b7d8-e5e057f6f7b1",
+      "body": "{\"version\": \"0\", \"id\": \"c2739b79-e0c4-44c1-b241-b1be983ef146\", \"detail-type\": \"Object Created\", \"source\": \"aws.s3\", \"account\": \"123456789012\", \"time\": \"2026-01-27T00:00:00Z\", \"region\": \"us-east-1\", \"resources\": [\"arn:aws:s3:::local-config-bucket\"], \"detail\": {\"version\": \"0\", \"bucket\": {\"name\": \"local-config-bucket\"}, \"object\": {\"key\": \"RefinerInput/persistence/id\"}, \"size\": 123}}",
+      "attributes": {},
+      "messageAttributes": {},
+      "md5OfBody": "",
+      "eventSource": "aws:sqs",
+      "eventSourceARN": "arn:aws:sqs:us-east-1:123456789012:local-queue",
+      "awsRegion": "us-east-1"
+    }
+  ]
+}
+```
---
+++ refiner/app/lambda/test_lambda.py
@@ -0,0 +1,371 @@
+import json
+from pathlib import Path
+from zipfile import ZipFile
+
+import boto3
+import pytest
+from moto import mock_aws
+
+from ..services.file_io import get_asset_path
+
+
+@pytest.fixture
+def aws_mock():
+    with mock_aws():
+        yield
+
+
+@pytest.fixture
+def config_bucket_env(monkeypatch) -> str:
+    bucket_name = "config-bucket"
+    monkeypatch.setenv("S3_BUCKET_CONFIG", bucket_name)
+    return bucket_name
+
+
+@pytest.fixture
+def lambda_event() -> dict:
+    event_file_path = Path(__file__).parent / "example-events" / "event.json"
+    with open(event_file_path) as f:
+        return json.load(f)
+
+
+@pytest.fixture
+def sample_xml_files() -> dict[str, bytes]:
+    zip_path = get_asset_path("demo", "mon-mothma-reportable-two-jds.zip")
+
+    with ZipFile(zip_path) as z:
+        with z.open("CDA_RR.xml") as f:
+            rr_xml = f.read()
+        with z.open("CDA_eICR.xml") as f:
+            eicr_xml = f.read()
+
+    return {
+        "rr_xml": rr_xml,
+        "eicr_xml": eicr_xml,
+    }
+
+
+@pytest.fixture
+def s3_client(aws_mock):
+    return boto3.client("s3", region_name="us-east-1")
+
+
+@pytest.fixture
+def data_bucket(s3_client) -> str:
+    bucket_name = "dibbs-refiner-dev"
+    s3_client.create_bucket(Bucket=bucket_name)
+    return bucket_name
+
+
+@pytest.fixture
+def config_bucket(s3_client, config_bucket_env) -> str:
+    s3_client.create_bucket(Bucket=config_bucket_env)
+    return config_bucket_env
+
+
+@pytest.fixture
+def s3_input_objects(
+    s3_client,
+    data_bucket,
+    sample_xml_files,
+):
+    persistence_id = "persistence/id"
+
+    s3_client.put_object(
+        Bucket=data_bucket,
+        Key=f"RefinerInput/{persistence_id}",
+        Body=sample_xml_files["rr_xml"],
+    )
+
+    s3_client.put_object(
+        Bucket=data_bucket,
+        Key=f"eCRMessageV2/{persistence_id}",
+        Body=sample_xml_files["eicr_xml"],
+    )
+
+    return persistence_id
+
+
+def collect_lambda_output_keys(s3_client, bucket) -> list[str]:
+    """
+    Returns a list of all keys written to the S3 bucket.
+    """
+    return [obj["Key"] for obj in s3_client.list_objects_v2(Bucket=bucket)["Contents"]]
+
+
+def get_refiner_complete_content(s3_client, bucket, persistance_id) -> dict:
+    """
+    Returns the content of the RefinerComplete file as a dict
+    """
+    resp = s3_client.get_object(Bucket=bucket, Key=f"RefinerComplete/{persistance_id}")
+    return json.loads(resp["Body"].read().decode("utf-8"))
+
+
+@mock_aws
+def test_lambda_inactive(
+    lambda_event,
+    s3_client,
+    data_bucket,
+    config_bucket,
+    s3_input_objects,
+):
+    """
+    Test that a file with two reportable conditions works when configurations are not
+    present for either condition.
+    """
+    from .lambda_function import lambda_handler
+
+    # Run the Lambda
+    response = lambda_handler(lambda_event, context={})
+    assert response["statusCode"] == 200
+
+    # Collect names of all keys that exist after running the lambda
+    created_files = collect_lambda_output_keys(s3_client=s3_client, bucket=data_bucket)
+
+    # Expected input files paths
+    assert "RefinerInput/persistence/id" in created_files
+    assert "eCRMessageV2/persistence/id" in created_files
+
+    # Expected eICR output files created by Refiner
+    full_flu_path = "RefinerOutput/persistence/id/SDDH/772828001/refined_eICR.xml"
+    full_covid_path = "RefinerOutput/persistence/id/SDDH/840539006/refined_eICR.xml"
+    # TODO: swap these out with the actual value once we get it from APHL
+    full_shadow_rr_path = (
+        "RefinerOutput/persistence/id/SDDH/inactive-codes/refined_RR.xml"
+    )
+
+    # Skipped due to no current.json files found
+    assert full_flu_path not in created_files
+    assert full_covid_path not in created_files
+    assert full_shadow_rr_path in created_files
+
+    # Expected completion file for pipeline
+    assert "RefinerComplete/persistence/id" in created_files
+
+    # Load RefinerComplete/persistence/id and ensure it contains expected paths
+    complete_json = get_refiner_complete_content(
+        s3_client=s3_client, bucket=data_bucket, persistance_id=s3_input_objects
+    )
+
+    # Neither condition was refined
+    expected_refiner_metadata = {"SDDH": {"840539006": False, "772828001": False}}
+    assert complete_json["RefinerMetadata"] == expected_refiner_metadata
+
+    assert not complete_json["RefinerSkip"]
+
+    # Skipped due to no current.json files found
+    assert full_flu_path not in complete_json["RefinerOutputFiles"]
+    assert full_covid_path not in complete_json["RefinerOutputFiles"]
+
+
+@mock_aws
+def test_lambda_one_active(
+    lambda_event,
+    s3_client,
+    data_bucket,
+    config_bucket,
+    s3_input_objects,
+):
+    """
+    Test that a file with two reportable conditions works when a configuration is
+    active for only one of those conditions.
+    """
+    from .lambda_function import lambda_handler
+
+    # COVID = 840539006
+    # Flu = 772828001
+
+    # Create current.json for COVID
+    covid_current = {"version": 1}
+    s3_client.put_object(
+        Bucket=config_bucket,
+        Key="configurations/SDDH/840539006/current.json",
+        Body=json.dumps(covid_current, indent=2).encode("utf-8"),
+        ContentType="application/json",
+    )
+
+    # Create activation for COVID
+    covid_activation = {
+        "codes": ["101289-7"],
+        "sections": [],
+        "included_condition_rsg_codes": ["840539006"],
+    }
+    s3_client.put_object(
+        Bucket=config_bucket,
+        Key=f"configurations/SDDH/840539006/{covid_current['version']}/active.json",
+        Body=json.dumps(covid_activation, indent=2).encode("utf-8"),
+        ContentType="application/json",
+    )
+
+    # Run the Lambda
+    response = lambda_handler(lambda_event, context={})
+    assert response["statusCode"] == 200
+
+    # Check that expected output files were written
+    created_files = collect_lambda_output_keys(s3_client=s3_client, bucket=data_bucket)
+
+    full_shadow_rr_path = (
+        "RefinerOutput/persistence/id/SDDH/inactive-codes/refined_RR.xml"
+    )
+
+    assert f"RefinerComplete/{s3_input_objects}" in created_files
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/840539006/refined_eICR.xml"
+        in created_files
+    )
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/840539006/refined_RR.xml"
+        in created_files
+    )
+    # since one reportable condition didn't have an active config, a shadow RR
+    # should have gotten written
+    assert full_shadow_rr_path in created_files
+
+    # Check that content of RefinerComplete looks correct
+    complete_json = get_refiner_complete_content(
+        s3_client=s3_client, bucket=data_bucket, persistance_id=s3_input_objects
+    )
+
+    # Only COVID should be marked as refined
+    expected_refiner_metadata = {"SDDH": {"840539006": True, "772828001": False}}
+    assert complete_json["RefinerMetadata"] == expected_refiner_metadata
+
+    assert not complete_json["RefinerSkip"]
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/840539006/refined_eICR.xml"
+        in complete_json["RefinerOutputFiles"]
+    )
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/840539006/refined_RR.xml"
+        in complete_json["RefinerOutputFiles"]
+    )
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/772828001/refined_eICR.xml"
+        not in complete_json["RefinerOutputFiles"]
+    )
+
+
+@mock_aws
+def test_lambda_all_active(
+    lambda_event,
+    s3_client,
+    data_bucket,
+    config_bucket,
+    s3_input_objects,
+):
+    """
+    Test that a file with two reportable conditions works when a configuration is
+    active for both of those conditions.
+    """
+    from .lambda_function import lambda_handler
+
+    # COVID = 840539006
+    # Flu = 772828001
+
+    # Create current.json for COVID
+    covid_current = {"version": 1}
+    s3_client.put_object(
+        Bucket=config_bucket,
+        Key="configurations/SDDH/840539006/current.json",
+        Body=json.dumps(covid_current, indent=2).encode("utf-8"),
+        ContentType="application/json",
+    )
+    # Create activation for COVID
+    covid_activation = {
+        "codes": ["101289-7"],
+        "sections": [],
+        "included_condition_rsg_codes": ["840539006"],
+    }
+    s3_client.put_object(
+        Bucket=config_bucket,
+        Key=f"configurations/SDDH/840539006/{covid_current['version']}/active.json",
+        Body=json.dumps(covid_activation, indent=2).encode("utf-8"),
+        ContentType="application/json",
+    )
+
+    # Create current.json for the flu
+    flu_current = {"version": 1}
+    s3_client.put_object(
+        Bucket=config_bucket,
+        Key="configurations/SDDH/772828001/current.json",
+        Body=json.dumps(flu_current, indent=2).encode("utf-8"),
+        ContentType="application/json",
+    )
+    # Create activation for the flu
+    flu_activation = {
+        "codes": ["100343-3"],
+        "sections": [],
+        "included_condition_rsg_codes": ["772828001"],
+    }
+    s3_client.put_object(
+        Bucket=config_bucket,
+        Key=f"configurations/SDDH/772828001/{flu_current['version']}/active.json",
+        Body=json.dumps(flu_activation, indent=2).encode("utf-8"),
+        ContentType="application/json",
+    )
+
+    # Run the Lambda
+    response = lambda_handler(lambda_event, context={})
+    assert response["statusCode"] == 200
+
+    # Check that expected output files were written
+    created_files = collect_lambda_output_keys(s3_client=s3_client, bucket=data_bucket)
+    full_shadow_rr_path = (
+        "RefinerOutput/persistence/id/SDDH/inactive-codes/refined_RR.xml"
+    )
+    assert f"RefinerComplete/{s3_input_objects}" in created_files
+
+    # Expect COVID outputs
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/840539006/refined_eICR.xml"
+        in created_files
+    )
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/840539006/refined_RR.xml"
+        in created_files
+    )
+
+    # Expect flu outputs
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/772828001/refined_eICR.xml"
+        in created_files
+    )
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/772828001/refined_RR.xml"
+        in created_files
+    )
+
+    # Shouldn't have shadow RR since all reportable conditions have corresponding
+    # active configs
+    assert full_shadow_rr_path not in created_files
+
+    # Check that content of RefinerComplete looks correct
+    complete_json = get_refiner_complete_content(
+        s3_client=s3_client, bucket=data_bucket, persistance_id=s3_input_objects
+    )
+
+    # Both COVID and Flu should be marked as refined
+    expected_refiner_metadata = {"SDDH": {"840539006": True, "772828001": True}}
+    assert complete_json["RefinerMetadata"] == expected_refiner_metadata
+
+    assert not complete_json["RefinerSkip"]
+
+    # Expect COVID
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/840539006/refined_eICR.xml"
+        in complete_json["RefinerOutputFiles"]
+    )
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/840539006/refined_RR.xml"
+        in complete_json["RefinerOutputFiles"]
+    )
+
+    # Expect flu
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/772828001/refined_eICR.xml"
+        in complete_json["RefinerOutputFiles"]
+    )
+    assert (
+        f"RefinerOutput/{s3_input_objects}/SDDH/772828001/refined_RR.xml"
+        in complete_json["RefinerOutputFiles"]
+    )
