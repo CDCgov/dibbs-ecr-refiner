9f79148cd84e0d4152ba28154e04a9e1d8de5a02
Bob Zhao <bob@skylight.digital>
2026-03-18 15:47:58 -0400

    linting commit of shame

--- refiner/tests/unit/test_service_conditions.py
+++ refiner/tests/unit/test_service_conditions.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 import pytest
 
 from app.db.conditions.model import DbCondition
-from app.services.conditions import _get_computed_name, create_condition_mapping_payload
+from app.services.conditions import create_condition_mapping_payload
 
 
 def _make_db_condition(name: str, url: str, version: str, rsg_code: str):
