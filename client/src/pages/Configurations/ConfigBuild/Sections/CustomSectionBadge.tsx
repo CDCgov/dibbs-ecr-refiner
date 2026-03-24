7ac4723949d9263806323303614e3e6e55dc031a
Jake Wheeler <jake.wheeler@skylight.digital>
2026-03-24 17:25:41 -0400

    useless imports

--- client/src/pages/Configurations/ConfigBuild/Sections/CustomSectionBadge.tsx
+++ client/src/pages/Configurations/ConfigBuild/Sections/CustomSectionBadge.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export function CustomSectionBadge() {
   return (
     <span className="bg-gray-cool-3 flex items-center justify-center rounded-sm px-2 py-0.5 text-sm">
