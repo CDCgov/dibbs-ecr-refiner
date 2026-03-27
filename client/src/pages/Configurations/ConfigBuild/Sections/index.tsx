986d8fdcc5c74e05a3fed1a822dc5a942292d26a
Jake Wheeler <jake.wheeler@skylight.digital>
2026-03-27 15:21:14 -0400

    duplicate checkbox code

--- client/src/pages/Configurations/ConfigBuild/Sections/index.tsx
+++ client/src/pages/Configurations/ConfigBuild/Sections/index.tsx
@@ -306,20 +306,7 @@ function IncludeCheckbox({
       onChange={(checked) => {
         updateSection(currentSection, { include: checked });
       }}
-    >
-      <svg
-        className="group-data-checked:bg-blue-cool-50 hidden stroke-white group-data-checked:block"
-        viewBox="0 0 14 14"
-        fill="none"
-      >
-        <path
-          d="M3 8L6 11L11 3.5"
-          strokeWidth={2}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    </Checkbox>
+    />
   );
 }
 
