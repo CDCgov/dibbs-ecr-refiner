1f5c6a7360dcb8221807bc48672a381876fa3c79
Jake Wheeler <jake.wheeler@skylight.digital>
2026-04-17 10:46:38 -0400

    navigation check

--- client/e2e/activity-log.spec.ts
+++ client/e2e/activity-log.spec.ts
@@ -10,10 +10,13 @@ test.describe('Activity log', () => {
     await deleteAllConfigurations();
   });
 
-  test('Check empty state', async ({ activityLogPage }) => {
+  test('Check empty state', async ({ activityLogPage, page }) => {
     await activityLogPage.goto();
     const rowData = await activityLogPage.getTableRows();
     expect(rowData).toHaveLength(0);
+    await expect(
+      page.getByRole('navigation', { name: 'Pagintion' }).getByRole('button')
+    ).toHaveCount(1);
   });
 
   test('Check entries from configuration creation', async ({
