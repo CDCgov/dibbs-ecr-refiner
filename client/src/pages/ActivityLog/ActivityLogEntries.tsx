aaa6914013194b6ba3bd7026b1afc535f9777c15
Jake Wheeler <jake.wheeler@skylight.digital>
2026-04-28 13:51:22 -0400

    add scope to header elements

--- client/src/pages/ActivityLog/ActivityLogEntries.tsx
+++ client/src/pages/ActivityLog/ActivityLogEntries.tsx
@@ -131,9 +131,11 @@ function ViewAllCustomCodeEventsButton({
                 <table className="w-full table-fixed">
                   <thead>
                     <tr className="border-gray-cool-20 text-gray-cool-90 border-b">
-                      <th className="py-3">Code system</th>
-                      <th>Code</th>
-                      <th>Display name</th>
+                      <th scope="col" className="py-3">
+                        Code system
+                      </th>
+                      <th scope="col">Code</th>
+                      <th scope="col">Display name</th>
                     </tr>
                   </thead>
                   <tbody className="divide-gray-cool-20 divide-y">
