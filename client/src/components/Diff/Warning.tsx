e93ba24ec2738408870c303a32ded806b8ab2921
Bob Zhao <bob@skylight.digital>
2026-04-30 11:48:04 -0400

    lint

--- client/src/components/Diff/Warning.tsx
+++ client/src/components/Diff/Warning.tsx
@@ -8,10 +8,7 @@ type WarningProps = {
 export function Warning({ heading, message }: WarningProps) {
   return (
     <div className="text-state-error-dark bg-state-error-lighter flex w-100 items-start p-4">
-      <Icon.Warning
-        aria-hidden
-        className="-mt-0.5 h-5! w-7!"
-      />
+      <Icon.Warning aria-hidden className="-mt-0.5 h-5! w-7!" />
       <div className="ml-4">
         <div className="mb-2 font-bold"> {heading}</div>
         <p>{message}</p>
