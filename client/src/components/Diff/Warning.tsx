d865afc9827524a21158e28917b6a854dc0b1feb
fzhao99 <bob@skylight.digital>
2026-04-30 10:58:17 -0400

    improve component suggestions
    Co-authored-by: Jake Wheeler <jakewheeler17@gmail.com>

--- client/src/components/Diff/Warning.tsx
+++ client/src/components/Diff/Warning.tsx
@@ -1,17 +1,17 @@
 import { Icon } from '@trussworks/react-uswds';
 
-type Warning = {
+type WarningProps = {
   heading: string;
   message: string;
 };
 
-export function Warning({ heading, message }: Warning) {
+export function Warning({ heading, message }: WarningProps) {
   return (
     <div className="text-state-error-dark bg-state-error-lighter flex w-100 items-start p-4">
       <Icon.Warning
-        aria-label="Warning icon to visually indicate issue with the page"
+        aria-hidden
         className="-mt-0.5 h-5! w-7!"
-      ></Icon.Warning>
+      />
       <div className="ml-4">
         <div className="mb-2 font-bold"> {heading}</div>
         <p>{message}</p>
