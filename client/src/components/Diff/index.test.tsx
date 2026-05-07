c7f34d49a9a3fee3c363787498a394033cc30e4c
Bob Zhao <bob@skylight.digital>
2026-05-07 16:58:16 -0400

    sigh

--- client/src/components/Diff/index.test.tsx
+++ client/src/components/Diff/index.test.tsx
@@ -6,7 +6,6 @@ const mockMatchedCondition: Condition = {
   code: '840539006',
   display_name: 'COVID-19',
   refined_eicr: '<xml>refined covid</xml>',
-  refined_rr: '<xml>refined covid</xml>',
   stats: ['eICR file reduced by 71%'],
   render_diff: true,
 };
