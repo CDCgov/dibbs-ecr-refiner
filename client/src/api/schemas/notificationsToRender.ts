ecf5d706e6faca8a0a5d7aa82e75fcfeccbe9be4
Jake Wheeler <jake.wheeler@skylight.digital>
2026-05-19 09:53:44 -0400

    newly generated files, fix typing issue

--- client/src/api/schemas/notificationsToRender.ts
+++ client/src/api/schemas/notificationsToRender.ts
@@ -1,8 +1,8 @@
-import type { NotificationsToRenderToRender } from './notificationsToRenderToRender';
+import type { NotificationKeys } from './notificationKeys';
 
 /**
  * Map of booleans for each of the notificaiton keys as to whether to render frontend banners.
  */
 export interface NotificationsToRender {
-  to_render: NotificationsToRenderToRender;
+  to_render: Partial<Record<NotificationKeys, boolean>>;
 }
