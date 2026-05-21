0d737aa3ec396cc01ba04d9b5980e69a164bff62
Bob Zhao <bob@skylight.digital>
2026-05-21 11:21:58 -0400

    a comma

--- refiner/migrations/20260520185510_add_system_table.sql
+++ refiner/migrations/20260520185510_add_system_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE systems (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
     key UNIQUE text NOT NULL,
     display_name UNIQUE text NOT NULL,
-    oid text UNIQUE
+    oid text UNIQUE,
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz NOT NULL DEFAULT now(),
 
