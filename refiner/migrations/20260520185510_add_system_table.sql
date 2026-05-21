56721a45034a3fdc606f553cc69b2100ca9a21b1
Bob Zhao <bob@skylight.digital>
2026-05-21 11:20:58 -0400

    add constraints to table

--- refiner/migrations/20260520185510_add_system_table.sql
+++ refiner/migrations/20260520185510_add_system_table.sql
@@ -1,9 +1,12 @@
 -- migrate:up
 CREATE TABLE systems (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-    key text NOT NULL,
-    display_name text NOT NULL,
+    key UNIQUE text NOT NULL,
+    display_name UNIQUE text NOT NULL,
     oid text UNIQUE
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now(),
+
 );
 
 -- migrate:down
