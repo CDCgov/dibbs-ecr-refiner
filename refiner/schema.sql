c34eb59b932c91f764029ca290f411bb795dd7b2
Bob Zhao <bob@skylight.digital>
2026-06-09 10:47:13 -0400

    schema

--- refiner/schema.sql
+++ refiner/schema.sql
@@ -119,6 +119,31 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: codes; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.codes (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    name text NOT NULL,
+    value text NOT NULL,
+    version text NOT NULL,
+    system_id uuid NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+--
+-- Name: condition_child_rsg_codes; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.condition_child_rsg_codes (
+    code_id uuid NOT NULL,
+    condition_id uuid NOT NULL
+);
+
+
+--
 -- Name: conditions; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -220,6 +245,20 @@ CREATE TABLE public.configurations_sections (
 
 
 --
+-- Name: custom_codes; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.custom_codes (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    name text NOT NULL,
+    value text NOT NULL,
+    system_id uuid NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+--
 -- Name: events; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -309,6 +348,30 @@ CREATE TABLE public.users (
 
 
 --
+-- Name: codes codes_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.codes
+    ADD CONSTRAINT codes_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: codes codes_system_id_version_value_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.codes
+    ADD CONSTRAINT codes_system_id_version_value_key UNIQUE (system_id, version, value);
+
+
+--
+-- Name: condition_child_rsg_codes condition_child_rsg_codes_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.condition_child_rsg_codes
+    ADD CONSTRAINT condition_child_rsg_codes_pkey PRIMARY KEY (condition_id, code_id);
+
+
+--
 -- Name: conditions conditions_canonical_url_version_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -386,6 +449,22 @@ ALTER TABLE ONLY public.configurations_sections
 
 ALTER TABLE ONLY public.configurations_sections
     ADD CONSTRAINT configurations_sections_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: custom_codes custom_codes_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.custom_codes
+    ADD CONSTRAINT custom_codes_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: custom_codes custom_codes_system_id_value_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.custom_codes
+    ADD CONSTRAINT custom_codes_system_id_value_key UNIQUE (system_id, value);
 
 
 --
@@ -534,6 +613,13 @@ CREATE TRIGGER configurations_set_last_activated_at_on_status_change_trigger BEF
 
 
 --
+-- Name: codes update_codes_updated_at; Type: TRIGGER; Schema: public; Owner: -
+--
+
+CREATE TRIGGER update_codes_updated_at BEFORE UPDATE ON public.codes FOR EACH ROW EXECUTE FUNCTION public.set_updated_at();
+
+
+--
 -- Name: conditions_context_groupers update_conditions_context_groupers_updated_at; Type: TRIGGER; Schema: public; Owner: -
 --
 
@@ -562,6 +648,13 @@ CREATE TRIGGER update_configurations_updated_at BEFORE UPDATE ON public.configur
 
 
 --
+-- Name: custom_codes update_custom_codes_updated_at; Type: TRIGGER; Schema: public; Owner: -
+--
+
+CREATE TRIGGER update_custom_codes_updated_at BEFORE UPDATE ON public.custom_codes FOR EACH ROW EXECUTE FUNCTION public.set_updated_at();
+
+
+--
 -- Name: systems update_systems_updated_at; Type: TRIGGER; Schema: public; Owner: -
 --
 
@@ -576,11 +669,27 @@ CREATE TRIGGER update_users_updated_at BEFORE UPDATE ON public.users FOR EACH RO
 
 
 --
+-- Name: condition_child_rsg_codes condition_child_rsg_codes_code_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.condition_child_rsg_codes
+    ADD CONSTRAINT condition_child_rsg_codes_code_id_fkey FOREIGN KEY (code_id) REFERENCES public.codes(id) ON DELETE CASCADE;
+
+
+--
+-- Name: condition_child_rsg_codes condition_child_rsg_codes_condition_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.condition_child_rsg_codes
+    ADD CONSTRAINT condition_child_rsg_codes_condition_id_fkey FOREIGN KEY (condition_id) REFERENCES public.conditions(id) ON DELETE CASCADE;
+
+
+--
 -- Name: conditions_context_groupers conditions_context_groupers_condition_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.conditions_context_groupers
-    ADD CONSTRAINT conditions_context_groupers_condition_id_fkey FOREIGN KEY (condition_id) REFERENCES public.conditions(id) ON DELETE CASCADE;
+    ADD CONSTRAINT conditions_context_groupers_condition_id_fkey FOREIGN KEY (condition_id) REFERENCES public.conditions(id) ON UPDATE CASCADE ON DELETE RESTRICT;
 
 
 --
@@ -588,7 +697,7 @@ ALTER TABLE ONLY public.conditions_context_groupers
 --
 
 ALTER TABLE ONLY public.configurations_conditions
-    ADD CONSTRAINT configurations_conditions_condition_id_fkey FOREIGN KEY (condition_id) REFERENCES public.conditions(id);
+    ADD CONSTRAINT configurations_conditions_condition_id_fkey FOREIGN KEY (condition_id) REFERENCES public.conditions(id) ON UPDATE CASCADE ON DELETE RESTRICT;
 
 
 --
@@ -648,11 +757,27 @@ ALTER TABLE ONLY public.configurations_sections
 
 
 --
+-- Name: custom_codes custom_codes_system_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.custom_codes
+    ADD CONSTRAINT custom_codes_system_id_fkey FOREIGN KEY (system_id) REFERENCES public.systems(id) ON DELETE CASCADE;
+
+
+--
 -- Name: events_custom_code_uploads events_custom_code_uploads_event_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.events_custom_code_uploads
     ADD CONSTRAINT events_custom_code_uploads_event_id_fkey FOREIGN KEY (event_id) REFERENCES public.events(id) ON DELETE CASCADE;
+
+
+--
+-- Name: codes fk_codes_system_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.codes
+    ADD CONSTRAINT fk_codes_system_id_fkey FOREIGN KEY (system_id) REFERENCES public.systems(id) ON DELETE CASCADE;
 
 
 --
@@ -712,4 +837,5 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20260511160133'),
     ('20260520185510'),
     ('20260526153052'),
-    ('20260602161536');
+    ('20260602161536'),
+    ('20260604134336');
