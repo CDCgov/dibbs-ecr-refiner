9f40b661071ca5ac52c560ce525746a1681a1478
Bob Zhao <bob@skylight.digital>
2026-06-11 10:51:34 -0400

    schema refresh

--- refiner/schema.sql
+++ refiner/schema.sql
@@ -16,6 +16,20 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+--
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON SCHEMA public IS '';
+
+
+--
 -- Name: configuration_status; Type: TYPE; Schema: public; Owner: -
 --
 
@@ -825,4 +839,5 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20260520185510'),
     ('20260526153052'),
     ('20260602161536'),
+    ('20260604134336'),
     ('20260608203714');
