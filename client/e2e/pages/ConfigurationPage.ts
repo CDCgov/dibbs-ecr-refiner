b17eff051d921afb57d6cdf25ece9470ee8cc324
Bob Zhao <bob@skylight.digital>
2026-06-25 11:48:38 -0400

    fix label

--- client/e2e/pages/ConfigurationPage.ts
+++ client/e2e/pages/ConfigurationPage.ts
@@ -71,7 +71,7 @@ export class ConfigurationPage {
     await this.page
       .getByRole('button', { name: 'Add new custom code' })
       .click();
-    await this.page.getByLabel('Code').fill(code);
+    await this.page.getByLabel('Code', { exact: true }).fill(code);
     await this.page.getByLabel('Code system').selectOption(codeSystem);
     await this.page.getByLabel('Code name').fill(codeName);
     await this.page.getByRole('button', { name: 'Add custom code' }).click();
