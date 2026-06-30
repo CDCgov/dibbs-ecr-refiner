8ccd1444329609671d9f75133a324150fb058027
Jake Wheeler <jake.wheeler@skylight.digital>
2026-06-30 09:28:27 -0400

    wip success

--- client/src/components/Toast/index.tsx
+++ client/src/components/Toast/index.tsx
@@ -1,4 +1,5 @@
 import { Alert, AlertProps, HeadingLevel } from '@trussworks/react-uswds';
+import classNames from 'classnames';
 
 type ToastProps = {
   variant: AlertProps['type'];
@@ -24,11 +25,20 @@ export function Toast({
 }: ToastProps) {
   return (
     <div className="flex flex-col">
-      <div className="w-full mt-4 px-5">
-        <h4 className='text-lg font-bold ml-10'>{heading}</h4>
-        <p className='ml-10'>
-          {body}
-        </p>
+      <div
+        className={classNames('mt-4 flex w-full flex-col gap-2 px-5', {
+          'bg-state-success-lighter': variant === 'success',
+        })}
+      >
+        <div className="flex gap-1">
+          <CircleCheckIcon />
+          {heading ? (
+            <h4 className="ml-2 text-2xl font-bold whitespace-nowrap text-black">
+              {heading}
+            </h4>
+          ) : null}
+        </div>
+        <p className="ml-11 text-black">{body}</p>
       </div>
       <Alert
         className="w-full"
@@ -45,5 +55,20 @@ export function Toast({
         {body}
       </Alert>
     </div>
+  );
+}
+
+function CircleCheckIcon() {
+  return (
+    <svg
+      aria-hidden
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      className="shrink-0"
+    >
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
+    </svg>
   );
 }
