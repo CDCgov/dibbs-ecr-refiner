8777c3c2cf1c9c25c8749d1b0f6adb03d434d5e5
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2026-07-14 15:29:55 -0500

    Update refiner/migrations/20260709201220_drop_legacy_rsg_codes_column.sql
    Co-authored-by: Jake Wheeler <jake.wheeler@skylight.digital>

--- refiner/migrations/20260709201220_drop_legacy_rsg_codes_column.sql
+++ refiner/migrations/20260709201220_drop_legacy_rsg_codes_column.sql
@@ -33,5 +33,5 @@ ALTER TABLE conditions DROP COLUMN IF EXISTS child_rsg_snomed_codes;
 -- Since dropping a column is destructive, we cannot easily restore the data
 -- without a backup. We will leave the down migration empty or implement a
 -- basic column restoration if the schema allows.
-ALTER TABLE conditions ADD COLUMN child_rsg_snomed_codes JSONB;
+ALTER TABLE conditions ADD COLUMN child_rsg_snomed_codes TEXT[];
 CREATE INDEX idx_conditions_child_snomed_codes ON conditions USING GIN (child_rsg_snomed_codes);
