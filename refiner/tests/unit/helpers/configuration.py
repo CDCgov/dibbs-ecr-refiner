347ac49287ca745a30fab6ce766ea04b7f405c06
Jake Wheeler <jake.wheeler@skylight.digital>
2026-07-20 12:51:01 -0400

    unit test fixes

--- refiner/tests/unit/helpers/configuration.py
+++ refiner/tests/unit/helpers/configuration.py
@@ -4,14 +4,22 @@ from app.db.conditions.model import DbCondition
 from app.db.configurations.model import DbConfiguration
 from app.services.configurations import convert_config_to_storage_payload
 from app.services.terminology import ProcessedConfiguration
+from tests.unit.conftest import create_mock_systems
 
 
 async def create_processed_config(
     config: DbConfiguration, conditions: list[DbCondition]
 ):
-    with unittest.mock.patch(
-        "app.services.configurations.get_included_conditions_db",
-        return_value=conditions,
+    mock_systems = create_mock_systems()
+    with (
+        unittest.mock.patch(
+            "app.services.configurations.get_included_conditions_db",
+            return_value=conditions,
+        ),
+        unittest.mock.patch(
+            "app.services.configurations.get_all_code_systems_db",
+            new=unittest.mock.AsyncMock(return_value={m.id: m for m in mock_systems}),
+        ),
     ):
         storage_payload = await convert_config_to_storage_payload(
             configuration=config,
