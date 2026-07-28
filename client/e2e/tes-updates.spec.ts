b630001911099c864dbb28ecc3056f8388dba8e2
Bob Zhao <bob@skylight.digital>
2026-07-28 14:06:50 -0400

    leave the TES updates e2e in

--- client/e2e/tes-updates.spec.ts
+++ client/e2e/tes-updates.spec.ts
@@ -5,13 +5,13 @@ test.describe('TES updates page', () => {
     await tesUpdatesPage.goto();
   });
 
-  // test('Page is accessible and has expected content', async ({
-  //   makeAxeBuilder,
-  //   tesUpdatesPage,
-  // }) => {
-  //   await tesUpdatesPage.goToTesUpdate('3.0.0');
-  //   await expect(makeAxeBuilder).toHaveNoAxeViolations();
+  test('Page is accessible and has expected content', async ({
+    makeAxeBuilder,
+    tesUpdatesPage,
+  }) => {
+    await tesUpdatesPage.goToTesUpdate('3.0.0');
+    await expect(makeAxeBuilder).toHaveNoAxeViolations();
 
-  //   await tesUpdatesPage.goToTesUpdate('6.0.0');
-  // });
+    await tesUpdatesPage.goToTesUpdate('6.0.0');
+  });
 });
