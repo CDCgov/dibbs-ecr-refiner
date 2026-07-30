49ab3bda4ac68b28bdc9a67954c1e9156839c552
Bob Zhao <bob@skylight.digital>
2026-07-30 09:05:57 -0400

    tweak the migration

--- refiner/schema.sql
+++ refiner/schema.sql
@@ -159,7 +159,8 @@ CREATE TABLE public.codes (
     code text CONSTRAINT codes_value_not_null NOT NULL,
     system_id uuid NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    version text
 );
 
 
@@ -946,5 +947,4 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20260701151910'),
     ('20260709201220'),
     ('20260716184236'),
-    ('20260728212408'),
-    ('20260729154745');
+    ('20260728212408');
