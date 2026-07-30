da0f97e6c1c18d9bd63fe8eb7aef1290b9ead2c9
Bob Zhao <bob@skylight.digital>
2026-07-30 10:28:56 -0400

    add an update to the up migration

--- refiner/migrations/20260729154745_drop_codes_version_col.sql
+++ refiner/migrations/20260729154745_drop_codes_version_col.sql
@@ -8,11 +8,19 @@ ALTER TABLE codes
 
 -- delete rows made duplicate with the dropped version so we can apply the 
 -- followup unique index
-DELETE FROM codes c1
-USING codes c2
-WHERE c1.id > c2.id
-    AND c1.system_id = c2.system_id
-    AND c1.code = c2.code;
+
+WITH duplicates_to_delete AS (
+    DELETE FROM codes c1
+    USING codes c2
+    WHERE c1.id > c2.id
+      AND c1.system_id = c2.system_id
+      AND c1.code = c2.code
+    RETURNING c1.id AS old_id, c2.id AS new_id
+)
+UPDATE conditions_codes cc
+SET cc.code_id = d.new_id
+FROM duplicates_to_delete d
+WHERE cc.code_id = d.old_id;
 
 ALTER TABLE codes
     ADD CONSTRAINT codes_system_id_code_value_key
@@ -31,7 +39,7 @@ WITH ranked_codes AS (
     SELECT 
       c.id as code_id,
       tes.version,
-      ROW_NUMBER() OVER (PARTITION BY c.id ORDER BY tes.version ASC) as rn 
+      DENSE_RANK() OVER (PARTITION BY cc.code_id ORDER BY tes.version ASC) as rn
     FROM conditions_codes cc 
     JOIN codes c ON cc.code_id = c.id 
     JOIN conditions cond ON cc.condition_id = cond.id 
@@ -50,22 +58,15 @@ ALTER TABLE codes
         UNIQUE (system_id, version, code);
 
 -- -- -- insert all the other ones
-WITH ranked_versions AS (
-    SELECT 
-      cc.code_id,
-      tes.version,
-      DENSE_RANK() OVER (PARTITION BY cc.code_id ORDER BY tes.version ASC) as rk 
-    FROM conditions_codes cc 
-    JOIN conditions cond ON cc.condition_id = cond.id 
-    JOIN tes ON cond.tes_id = tes.id
-)
 INSERT INTO codes (system_id, code, version, display, created_at)
-SELECT DISTINCT
+SELECT
     c.system_id,
     c.code,
-    rv.version,
+    t.version,
     c.display,
     c.created_at
-FROM ranked_versions rv
-JOIN codes c ON c.id = rv.code_id
-WHERE rv.rk > 1;
+FROM conditions_codes cc
+LEFT JOIN codes c ON c.id = cc.code_id 
+LEFT JOIN conditions cond ON cc.condition_id = cond.id
+LEFT JOIN tes t ON cond.tes_id = t.id 
+ON CONFLICT (system_id, version, code) DO NOTHING;
