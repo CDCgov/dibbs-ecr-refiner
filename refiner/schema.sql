6b52ae18ed4af62eff9595027a1d1600507c8927
Bob Zhao <bob@skylight.digital>
2026-07-30 14:09:12 -0400

    add migration to drop codes

--- refiner/schema.sql
+++ refiner/schema.sql
@@ -138,7 +138,6 @@ CREATE TABLE public.codes (
     id uuid DEFAULT gen_random_uuid() NOT NULL,
     display text CONSTRAINT codes_name_not_null NOT NULL,
     code text CONSTRAINT codes_value_not_null NOT NULL,
-    version text NOT NULL,
     system_id uuid NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     updated_at timestamp with time zone DEFAULT now() NOT NULL
@@ -381,11 +380,11 @@ ALTER TABLE ONLY public.codes
 
 
 --
--- Name: codes codes_system_id_version_value_key; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: codes codes_system_id_code_value_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.codes
-    ADD CONSTRAINT codes_system_id_version_value_key UNIQUE (system_id, version, code);
+    ADD CONSTRAINT codes_system_id_code_value_key UNIQUE (system_id, code);
 
 
 --
@@ -602,13 +601,6 @@ ALTER TABLE ONLY public.users
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_username_key UNIQUE (username);
-
-
---
--- Name: codes_upsert_constraint_idx; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE UNIQUE INDEX codes_upsert_constraint_idx ON public.codes USING btree (system_id, version, code);
 
 
 --
@@ -912,4 +904,6 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20260630173611'),
     ('20260701151910'),
     ('20260709201220'),
-    ('20260716184236');
+    ('20260716184236'),
+    ('20260728212408'),
+    ('20260729154745');
