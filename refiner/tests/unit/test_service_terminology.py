a367e662bec0a4f7192b128f48ee12887e23fbda
Jake Wheeler <jake.wheeler@skylight.digital>
2026-08-03 14:37:12 -0400

    fix unit tests

--- refiner/tests/unit/test_service_terminology.py
+++ refiner/tests/unit/test_service_terminology.py
@@ -59,12 +59,12 @@ def mock_db_functions(monkeypatch, mock_all_systems):
     Mock return values of the `_db` functions called by the routes.
     """
     monkeypatch.setattr(
-        "app.services.code_systems.get_all_code_systems_db",
+        "app.services.code_systems.get_id_to_code_system_dict_db",
         AsyncMock(return_value={m.id: m for m in mock_all_systems}),
     )
 
     monkeypatch.setattr(
-        "app.services.configurations.get_all_code_systems_db",
+        "app.services.configurations.get_id_to_code_system_dict_db",
         AsyncMock(return_value={m.id: m for m in mock_all_systems}),
     )
 
