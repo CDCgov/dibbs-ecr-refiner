08825096daaaf27f145c7e6582ce94e07cc30ba6
fzhao99 <bob@skylight.digital>
2026-08-03 10:50:02 -0400

    chore: add precommit whitespace whitespace check to CI (#1631)
    Co-authored-by: fzhao99 <29645040+fzhao99@users.noreply.github.com>

--- refiner/migrations/20260728212408_update_conditions_join.sql
+++ refiner/migrations/20260728212408_update_conditions_join.sql
@@ -1,7 +1,7 @@
 -- migrate:up
 ALTER TABLE conditions_rsg_codes RENAME TO conditions_codes;
 ALTER TABLE conditions_codes ADD COLUMN is_child_rsg BOOLEAN DEFAULT FALSE;
-CREATE UNIQUE INDEX IF NOT EXISTS codes_upsert_constraint_idx 
+CREATE UNIQUE INDEX IF NOT EXISTS codes_upsert_constraint_idx
     ON codes (system_id, version, code);
 
 -- migrate:down
