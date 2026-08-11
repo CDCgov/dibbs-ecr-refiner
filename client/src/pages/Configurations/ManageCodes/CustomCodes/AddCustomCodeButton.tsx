49f1e51d776493bc7111e05818a20a07787bea92
Jake Wheeler <jake.wheeler@skylight.digital>
2026-08-11 10:12:19 -0400

    custom code button styling

--- client/src/pages/Configurations/ManageCodes/CustomCodes/AddCustomCodeButton.tsx
+++ client/src/pages/Configurations/ManageCodes/CustomCodes/AddCustomCodeButton.tsx
@@ -1,6 +1,7 @@
 import { Button, ButtonProps } from '@components/Button';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { CustomCodeModal } from './CustomCodeModal';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 
 type AddCustomCodeButtonProps = Pick<ButtonProps, 'disabled'> & {
   configurationId: string;
@@ -13,19 +14,41 @@ export function AddCustomCodeButton({
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <Button
-        onClick={() => setIsOpen(true)}
-        variant="unstyled"
-        className="text-violet-warm-60 flex h-8 flex-row items-center gap-2 rounded-md border-2! bg-white px-2 text-sm! font-bold hover:cursor-pointer hover:bg-[#f9f4f9]!"
-        aria-label="Add new custom code"
-        disabled={disabled}
-      >
-        <span>Add custom code</span>
-        <span aria-hidden className="flex flex-row gap-3">
-          <span className="opacity-40">|</span>
-          <DownArrow />
-        </span>
-      </Button>
+      <Menu>
+        <MenuButton
+          className="text-violet-warm-60 flex h-8 flex-row items-center gap-2 rounded-md border-2! bg-white px-2 text-sm! font-bold hover:cursor-pointer hover:bg-[#f9f4f9]!"
+          aria-label="Add new custom code"
+          disabled={disabled}
+        >
+          <span>Add custom code</span>
+          <span aria-hidden className="flex flex-row gap-3">
+            <span className="opacity-40">|</span>
+            <DownArrow />
+          </span>
+        </MenuButton>
+        <MenuItems
+          anchor="bottom end"
+          className="ring-opacity-5 border-gray-cool-10 text-md mt-1 flex w-60 flex-col items-start rounded-md bg-white shadow-lg focus-within:outline-none!"
+        >
+          <MenuItem>
+            <MenuItemButton
+              onClick={() => setIsOpen(true)}
+              ariaLabel="Add new custom code"
+            >
+              Add a single code
+            </MenuItemButton>
+          </MenuItem>
+          <div aria-hidden className="bg-gray-cool-10 my-1 h-px w-full" />
+          <MenuItem>
+            <MenuItemButton
+              onClick={() => setIsOpen(true)}
+              ariaLabel="Add new custom code"
+            >
+              Import codes from CSV
+            </MenuItemButton>
+          </MenuItem>
+        </MenuItems>
+      </Menu>
       <CustomCodeModal
         configurationId={configurationId}
         isOpen={isOpen}
@@ -34,6 +57,24 @@ export function AddCustomCodeButton({
         selectedCustomCode={null}
       />
     </>
+  );
+}
+
+interface MenuItemButtonProps {
+  onClick: () => void;
+  ariaLabel: string;
+  children: React.ReactNode;
+}
+function MenuItemButton({ onClick, ariaLabel, children }: MenuItemButtonProps) {
+  return (
+    <Button
+      className="hover:bg-blue-cool-5 flex w-full p-3 hover:cursor-pointer"
+      onClick={onClick}
+      variant="unstyled"
+      aria-label={ariaLabel}
+    >
+      {children}
+    </Button>
   );
 }
 
