b5bab073bac9ac697cb64cce8eeea33b24c21e82
Jake Wheeler <jake.wheeler@skylight.digital>
2026-08-13 11:20:50 -0400

    remove onClose

--- client/src/pages/Configurations/ManageCodes/Filters.tsx
+++ client/src/pages/Configurations/ManageCodes/Filters.tsx
@@ -29,7 +29,7 @@ function FilterCombobox<T extends FilterOption>({
   onChange,
 }: FilterComboboxProps<T>) {
   return (
-    <Combobox multiple value={selected} onChange={onChange} onClose={() => {}}>
+    <Combobox multiple value={selected} onChange={onChange}>
       <div className="relative">
         <ComboboxButton className="flex w-44 items-center justify-between gap-6 border border-gray-300 bg-white px-3 py-2 text-sm font-medium text-gray-800 shadow-sm hover:cursor-pointer focus:ring-2 focus:ring-blue-500 focus:outline-none">
           {selected.length <= 0 ? (
