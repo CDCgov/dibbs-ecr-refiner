b5d10edcbe9c002f33d308266ac4f2dbdca099b1
Bob Zhao <bob@skylight.digital>
2026-08-13 10:29:30 -0400

    update valueset migration

--- refiner/migrations/20260813142548_add_valueset_table.sql
+++ refiner/migrations/20260813142548_add_valueset_table.sql
@@ -9,8 +9,18 @@ CREATE TABLE IF NOT EXISTS valuesets (
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
+ALTER TABLE conditions_codes ADD COLUMN valueset_id UUID NOT NULL;
 
-
+ALTER TABLE conditions_codes
+    ADD CONSTRAINT fk_conditions_valuesets_fkey
+    FOREIGN KEY (valueset_id) 
+    REFERENCES valuesets (id)
+    ON DELETE CASCADE;
 
 -- migrate:down
+
+DROP CONSTRAINT fk_conditions_valuesets_fkey 
+ALTER TABLE conditions_codes DROP COLUMN valueset_id;
+DROP TABLE IF EXISTS valuesets;
+
 
