cd6540979f8a6800045636edebc9c49c2302c5f3
Bob Zhao <bob@skylight.digital>
2026-08-13 10:28:42 -0400

    add valueset table creation

--- refiner/migrations/20260813142548_add_valueset_table.sql
+++ refiner/migrations/20260813142548_add_valueset_table.sql
@@ -1,5 +1,16 @@
 -- migrate:up
 
+CREATE TABLE IF NOT EXISTS valuesets (
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
+    external_id text NOT NULL,
+    url text NOT NULL,
+    parent_url text,
+    name text NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
 
 -- migrate:down
 
