357b87eee42176aa9739f1a9479c425fbd6728fd
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2026-08-18 14:14:15 -0500

    refactor(test): remove redundant `waitFor`;
    Co-Authored-By: Jake Wheeler <jake.wheeler@skylight.digital>

--- client/e2e/pages/AppUpdatesPage.ts
+++ client/e2e/pages/AppUpdatesPage.ts
@@ -7,13 +7,11 @@ export class AppUpdatesPage {
     await this.page.goto('/');
 
     const settingsMenu = this.page.getByLabel('Open settings menu');
-    await settingsMenu.waitFor({ state: 'visible' });
     await settingsMenu.click();
 
     const appUpdatesItem = this.page.getByRole('menuitem', {
       name: 'App updates',
     });
-    await appUpdatesItem.waitFor({ state: 'visible' });
     await appUpdatesItem.click();
     await expect(
       this.page.getByRole('heading', {
