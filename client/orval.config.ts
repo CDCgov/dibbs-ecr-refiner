6e3d7cd265462d1d274f113105cc2bc046a40e87
Roger Steve Ruiz <roger.ruiz@skylight.digital>
2026-08-27 20:08:35 +0000

    feat(dx): ensure empty lines are removed from all generated files (#1711)
    Co-authored-by: Bob Zhao <bob@skylight.digital>

--- client/orval.config.ts
+++ client/orval.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
       },
     },
     hooks: {
-      afterAllFilesWrite: 'prettier --write',
+      afterAllFilesWrite: 'node ./scripts/run-afterAllFilesWrite.cjs',
     },
   },
 });
