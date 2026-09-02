aa8dc06622f4e15452f05191c39e9a638e7107f5
Jake Wheeler <jake.wheeler@skylight.digital>
2026-09-02 19:55:39 +0000

    feat: Code set drawer - primary code set displays lock icon (#1760)

--- client/src/pages/Configurations/ManageCodes/index.tsx
+++ client/src/pages/Configurations/ManageCodes/index.tsx
@@ -32,6 +32,7 @@ import { ControlPanel } from './ControlPanel';
 import { SearchBar } from './SearchBar';
 import { ImportCustomCodes } from './CustomCodes/CsvImport/ImportCustomCodes';
 import { Tooltip } from '@components/Tooltip';
+import { LockIcon } from './LockIcon';
 
 export function ManageCodes() {
   const { id } = useParams<{ id: string }>();
@@ -347,23 +348,6 @@ function CodesTable({
         </InfiniteScroll>
       </div>
     </div>
-  );
-}
-
-function LockIcon() {
-  return (
-    <svg
-      data-testid="lock-icon"
-      width="18"
-      height="18"
-      viewBox="0 0 24 24"
-      fill="#71767a"
-    >
-      <path
-        data-dc-tpl="743"
-        d="M18 8h-1V6A5 5 0 0 0 7 6v2H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2zM9 6a3 3 0 0 1 6 0v2H9V6zm3 11a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
-      />
-    </svg>
   );
 }
 
