81b487316e65d550fe6cdd68a7e7a9cc8137e6ae
Bob Zhao <bob@skylight.digital>
2026-09-22 10:24:29 -0400

    a comment

--- client/src/pages/Configurations/ManageCodes/ControlPanel.tsx
+++ client/src/pages/Configurations/ManageCodes/ControlPanel.tsx
@@ -564,6 +564,8 @@ function formatSelectedCodeCount(
     ).toString();
   }
 
+  // Handle the search string case separately since we do the count of the
+  // search results in the DB.
   return selectedCodeCount > CodesLimitResponseValue.codes_limit || hasNextPage
     ? `${CodesLimitResponseValue.codes_limit}+ codes`
     : selectedCodeCount.toString();
